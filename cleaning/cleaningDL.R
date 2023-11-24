16babfb1409d2e5dcbc233f5388471be8a929fa9
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2023-11-24 12:47:59 -0800

    DL cleaning

--- cleaning/cleaningDL.R
+++ cleaning/cleaningDL.R
@@ -2,7 +2,7 @@
 
 # Also to determine what datasets are still missing
 if(length(grep("deirdreloughnan", getwd()) > 0)) {
-  setwd("~/Documents/github/oegres")
+  setwd("~/Documents/github/egret")
 } else if(length(grep("Lizzie", getwd()) > 0)) {
   setwd("~/Documents/git/projects/others/deirdre/Synchrony")
 } else{
@@ -15,57 +15,75 @@ options(stringsAsFactors=FALSE)# Make sure words are read in as characters rathe
 ### Libraries
 library("readxl") # To read Excel files
 library(taxize) # To clean species names
+library(stringr)
 
 ### Import data##############################
 folder <- list.dirs(path = "data", full.names = FALSE, recursive = TRUE)
 folder <- folder[3:9] # Obtain the folders needed
 
 ### Merge Data##############################
-#Match cols:
-# DL
+#Deirdre 
 egret_DL.xlsx <- read_xlsx("data/egret_DL.xlsx", sheet = "data_detailed")
-egret_DL.xlsx$germ.tim.zero <- egret_DL.xlsx$germ.time.zero
-egret_DL.xlsx <- subset(egret_DL.xlsx, select = c("datasetID", "study", "entered.by", "genus", "species", "variety",
-                                                    "woody", "crop", "source.population", "provenance.lat", "provenance.long", "provenance.altitude",
-                                                    "continent", "no.indiv.collected", "year.collected", "year.germination", "storage.type", "storage.time",
-                                                    "storage.humidity", "storage.temp", "treatment", "chill.temp", "chill.duration", "germ.temp",
-                                                    "other.treatment", "photoperiod", "chemical", "chemcial.concent", "trt.duration", "scarification",
-                                                    "scarif.type", "soaking", "soaked.in", "soaking.duration", "seed.mass.given", "respvar",
-                                                    "response", "error.type", "resp.error", "reps", "n.per.rep", "germ.duration",
-                                                    "germ.tim.zero", "figure", "Notes"))
-# TA
-egret_TA.xlsx <- read_xlsx(paste("data/", "oegres_TA", "/", "oegres_TA.xlsx", sep = ""), sheet = "data_detailed")
-egret_TA.xlsx$Notes <- egret_TA.xlsx$notes
-egret_TA.xlsx <- subset(egret_TA.xlsx, select = c("datasetID", "study", "entered.by", "genus", "species", "variety",
-                                                    "woody", "crop", "source.population", "provenance.lat", "provenance.long", "provenance.altitude",
-                                                    "continent", "no.indiv.collected", "year.collected", "year.germination", "storage.type", "storage.time",
-                                                    "storage.humidity", "storage.temp", "treatment", "chill.temp", "chill.duration", "germ.temp",
-                                                    "other.treatment", "photoperiod", "chemical", "chemcial.concent", "trt.duration", "scarification",
-                                                    "scarif.type", "soaking", "soaked.in", "soaking.duration", "seed.mass.given", "respvar",
-                                                    "response", "error.type", "resp.error", "reps", "n.per.rep", "germ.duration",
-                                                    "germ.tim.zero", "figure", "Notes"))
+length(unique(egret_DL.xlsx$datasetID)) #7
 
-# SC
-egret_SC.xlsx <- read_xlsx(paste("data/", "oegres_SC", "/", "oegres_SC.xlsx", sep = ""), sheet = "data_detailed")
-egret_SC.xlsx$germ.tim.zero <- egret_SC.xlsx$germ.time.zero
-egret_SC.xlsx$Notes <- egret_SC.xlsx$notes
-egret_SC.xlsx <- subset(egret_SC.xlsx, select = c("datasetID", "study", "entered.by", "genus", "species", "variety",
-                                                    "woody", "crop", "source.population", "provenance.lat", "provenance.long", "provenance.altitude",
-                                                    "continent", "no.indiv.collected", "year.collected", "year.germination", "storage.type", "storage.time",
-                                                    "storage.humidity", "storage.temp", "treatment", "chill.temp", "chill.duration", "germ.temp",
-                                                    "other.treatment", "photoperiod", "chemical", "chemcial.concent", "trt.duration", "scarification",
-                                                    "scarif.type", "soaking", "soaked.in", "soaking.duration", "seed.mass.given", "respvar",
-                                                    "response", "error.type", "resp.error", "reps", "n.per.rep", "germ.duration",
-                                                    "germ.tim.zero", "figure", "Notes"))
-egret <- rbind(egret_DL.xlsx, egret_TA.xlsx, egret_SC.xlsx)
-folder <- folder[1:5]
-for(i in 1:length(folder)) {
-  file <- list.files(paste("data/", folder[i], sep = ""), pattern = NULL, all.files = TRUE, full.names = FALSE)
-  file <- file[3]
-  dataframe <- read_xlsx(paste("data/", folder[i], "/", file, sep = ""), sheet = "data_detailed")
-  egret <- rbind(egret, dataframe)
-  assign(file, dataframe)
-}
+# Tolu
+egret_TA.xlsx <- read_xlsx(paste("data/", "egret_TA", "/", "egret_TA.xlsx", sep = ""), sheet = "data_detailed")
+#colnames(egret_TA.xlsx)[colnames(egret_TA.xlsx) == "germ.tim.zero"] <- "germ.time.zero"
+colnames(egret_TA.xlsx)[colnames(egret_TA.xlsx) == "notes"] <- "Notes"
+length(unique(egret_TA.xlsx$datasetID)) #38
+
+# Sophia C
+egret_SC.xlsx <- read_xlsx(paste("data/", "egret_SC", "/", "egret_SC.xlsx", sep = ""), sheet = "data_detailed")
+#colnames(egret_SC.xlsx)[colnames(egret_SC.xlsx) == "germ.tim.zero"] <- "germ.time.zero"
+colnames(egret_SC.xlsx)[colnames(egret_SC.xlsx) == "notes"] <- "Notes"
+egret_SC.xlsx <- egret_SC.xlsx[complete.cases(egret_SC.xlsx$datasetID),]
+length(unique(egret_SC.xlsx$datasetID)) #35
+
+#Christophe
+egret_CRD.xlsx <- read_xlsx("data/egret_CRD.xlsx", sheet = "data_detailed")
+#colnames(egret_CRD.xlsx)[colnames(egret_CRD.xlsx) == "germ.tim.zero"] <- "germ.time.zero"
+length(unique(egret_CRD.xlsx$datasetID)) #4
+
+# Justin
+egret_JN.xlsx <- read_xlsx(paste("data/", "egret_JN", "/", "egret_JN_personal_3.11.2023.xlsx", sep = ""), sheet = "data_detailed")
+colnames(egret_JN.xlsx)[colnames(egret_JN.xlsx) == "germ.tim.zero"] <- "germ.time.zero"
+length(unique(egret_JN.xlsx$datasetID)) #58
+
+# Britany
+egret_BW.xlsx <- read_xlsx("data/egret_BW.xlsx", sheet = "data_detailed")
+#colnames(egret_BW.xlsx)[colnames(egret_BW.xlsx) == "germ.tim.zero"] <- "germ.time.zero"
+length(unique(egret_BW.xlsx$datasetID)) #7
+
+egret_MN.xlsx <- read_xlsx(paste("data/", "egret_MN", "/", "egret_MN.xlsx", sep = ""), sheet = "data_detailed")
+length(unique(egret_MN.xlsx$datasetID)) #50
+
+egret_HHN.xlsx <- read_xlsx(paste("data/", "egret_HHN", "/", "egret_HHN.xlsx", sep = ""), sheet = "data_detailed")
+length(unique(egret_HHN.xlsx$datasetID)) #26
+
+egret_DK.xlsx <- read_xlsx(paste("data/", "egret_DK", "/", "egret_DK.xlsx", sep = ""), sheet = "data_detailed")
+length(unique(egret_DK.xlsx$datasetID)) #10
+
+egret_DM.xlsx <- read_xlsx(paste("data/", "egret_DM", "/", "egret_DM.xlsx", sep = ""), sheet = "data_detailed")
+length(unique(egret_DM.xlsx$datasetID)) #27
+
+egret_GG.xlsx <- read_xlsx(paste("data/", "egret_GG", "/", "egret_GG.xlsx", sep = ""), sheet = "data_detailed")
+length(unique(egret_GG.xlsx$datasetID)) #12
+
+egret_AZ.xlsx <- read_xlsx(paste("data/", "egret_AZ", "/", "egret_AZ.xlsx", sep = ""), sheet = "data_detailed")
+length(unique(egret_AZ.xlsx$datasetID)) #12
+
+egret_JS.xlsx <- read_xlsx(paste("data/", "egret_JS", "/", "egret_JS.xlsx", sep = ""), sheet = "data_detailed")
+egret_JS.xlsx <- egret_JS.xlsx[complete.cases(egret_JS.xlsx$datasetID),] # tons of empty rows!  # 757 rows actual data
+length(unique(egret_JS.xlsx$datasetID)) #9
+
+egret1 <- rbind(egret_TA.xlsx, egret_BW.xlsx, egret_CRD.xlsx, egret_MN.xlsx, egret_HHN.xlsx, egret_DK.xlsx,
+                egret_JS.xlsx, egret_DM.xlsx, egret_AZ.xlsx, egret_GG.xlsx)
+colnames(egret1)[colnames(egret1) == "germ.tim.zero"] <- "germ.time.zero"
+egret2 <- rbind(egret_DL.xlsx, egret_SC.xlsx, egret1)
+
+colnames(egret2)[colnames(egret2) == "chemcial.concent"] <- "chemical.concent"
+egret <- rbind(egret_JN.xlsx, egret2)
+
 egret$datasetID <- tolower(egret$datasetID)
 egret$datasetID[which(egret$datasetID == "acosta12")] <- "acosta13"
 egret$datasetID[which(egret$datasetID == "brandel2005")] <- "brandel05"
@@ -77,8 +95,10 @@ egret$datasetID[which(egret$datasetID == "picciau18")] <- "picciau19"
 
 egret <- data.frame(egret)
 
-# General chekcs:
+# Full dataset: 30448      45 as of Nov 24 2023
+dim(egret)
 
+# General chekcs:
 #1. fix typos and minor issues:
 ### Clean Species ##############################
 egret$species <- tolower(egret$species)
@@ -89,6 +109,12 @@ egret_species <- unique(paste(egret$genus, egret$species))
 # egret_species_fix <- unique(fix_names$matched_name2)
 # names_changed <- setdiff(egret_species, egret_species_fix)
 # names_changed
+sort(egret_species)
+
+# Remove training spaces:
+egret$genus <- str_trim(egret$genus)
+egret$species <- str_trim(egret$species)
+
 # Fix#########################################
 egret$species[which(egret$genus == "Colutea" & egret$species == "bohsei")] <- "buhsei"
 egret$species[which(egret$genus == "Abies" & egret$species == "amabils")] <- "amabilis"
@@ -115,52 +141,99 @@ egret$species[which(egret$genus == "Jasminum" & egret$species == "fruiticans")] 
 
 egret$variety[which(egret$genus == "Pedicularis" & egret$species == "longiflora var.\r\ntubiformis")] <- "tubiformis"
 egret$species[which(egret$genus == "Pedicularis" & egret$species == "longiflora var.\r\ntubiformis")] <- "longiflora"
-# Confirm####################################
-egret_species <- unique(paste(egret$genus, egret$species))
-fix_names <- gnr_resolve(sci = egret_species, with_canonical_ranks = T)
-egret_species_fix <- unique(fix_names$matched_name2)
-names_changed <- setdiff(egret_species, egret_species_fix) # Confirm this is of length 0
-names_changed
-### TO CHECK: Echinacea angustifolia, purpurea, pallida -- 3 species in 1 study
-
+egret$species[which(egret$genus == "Ferula" & egret$species == "assa foetida")] <- "assa-foetida"
+egret$species[which(egret$genus == "Astrgalus" & egret$species == "cyclophyllon")] <- "cyclophyllus"
+egret$species[which(egret$genus == "Astrgalus" & egret$species == "cyclophyllu")] <- "cyclophyllus"
 egret$species[which(egret$species == "Amurensis")] <- "amurensis"
 egret$species[which(egret$species == "aviculare L.")] <- "aviculare"
 egret$species[which(egret$species == "longiflora var.\r\ntubiformis")] <- "longiflora"
 egret$species[which(egret$species == "Pagoda")] <- "pagoda"
 egret$species[which(egret$species == "Sylvestris L.")] <- "sylvestris"
 
-egret$sp.name <- paste(egret$genus, egret$species, sep = "_")
+# Confirm ####################################
+egret_species <- unique(paste(egret$genus, egret$species))
+fix_names <- gnr_resolve(sci = egret_species, with_canonical_ranks = T)
+egret_species_fix <- unique(fix_names$matched_name2)
+names_changed <- setdiff(egret_species, egret_species_fix) # Confirm this is of length 0
+names_changed
 
+### TO CHECK: Echinacea angustifolia, purpurea, pallida -- 3 species in 1 study
+egret$sp.name <- paste(egret$genus, egret$species, sep = "_")
+#View(sort(unique(egret$sp.name)))
 unique(egret$study)
 egret$study <- gsub(" ","", egret$study)
 
-unique(egret$variety) # only 18
+unique(egret$variety) # 49
 
 unique(egret$woody) # N, Y, NA, "rhizomatus shrub/bamboo", "succulent" 
+temp <- subset(egret, woody == "O") # Magnolia_ingrata aka Magnolia_fulva, yes woody
+egret$woody[which(egret$woody == "O")] <- "Y"
 
-unique(egret$source.population) #192 
+sort(unique(egret$source.population)) #410
 
 unique(egret$provenance.lat) # character bc includes 3 ranges and some wild decimals
 unique(egret$provenance.long)
 
 unique(egret$continent)
-egret$continent[which(egret$continent == "USA")] <- "North America" # 8 and some NA
+egret$continent[which(egret$continent == "USA")] <- "North America" 
+egret$continent[which(egret$continent == "North Ameria")] <- "North America" 
+# to check:
+#South Africa -- africa or south america?
+# Central america
+# America --- North america?
+# "NA, possibly Asia"
 
 unique(egret$no.indiv.collected) # only 13 reported values
-
+# some ranges -- some NA possibly...
 unique(egret$year.collected)
+egret$year.collected[which(egret$year.collected == "N/A")] <- "NA"
 #character bc 4 values entered as ranges, also from 1951-2019
 
 unique(egret$year.germination)
 egret$year.germination[which(egret$year.germination == "n/a")] <- "NA" #only 19 report this
 #TO CHECK - TRUE? But what is the value?
 
-unique(egret$storage.type)
+unique(egret$storage.type) # 75 options
+egret$storage.type[which(egret$storage.type == "N/A")] <- "NA"
+
+# Main categories: 
+#room temp
 egret$storage.type[which(egret$storage.type == "laboratory")] <- "room temp"
 egret$storage.type[which(egret$storage.type == "room conditions")] <- "room temp"
-egret$storage.type[which(egret$storage.type == "glass bottles, laboratory conditions")] <- "room temp"
-egret$storage.type[which(egret$storage.type == "dry/refrigerated")] <- "dry refrigeration"
-egret$storage.type[which(egret$storage.type == "dry refrigerator")] <- "dry refrigeration"
+egret$storage.type[which(egret$storage.type == "coin envelope (room temperature)")] <- "room temp"
+egret$storage.type[which(egret$storage.type == "ambient")] <- "room temp"
+
+egret$storage.type[which(egret$storage.type == "glass bottles, laboratory conditions")] <- "room temp air tight container"
+
+#Air tight container - no temp specified 
+egret$storage.type[which(egret$storage.type == "air-tight")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "air-tight containers")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "airtight plastic bags")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "airtight polyethylene bags")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "glass container")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "plastic bags")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "plastic bag")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "sealed containers")] <- "air-tight no temp spec"
+egret$storage.type[which(egret$storage.type == "sealed glass bottle")] <- "air-tight no temp spec"
+
+#Dry
+#Dry - cold
+egret$storage.type[which(egret$storage.type == "dry/refrigerated")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "dry refrigerator")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "dry, refrigerator")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "cold dry")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "cold, dry")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "cold/dry")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "dry/cold")] <- "dry cold"
+egret$storage.type[which(egret$storage.type == "dry/refrigerated")] <- "dry cold"
+
+# mosit -  cold
+egret$storage.type[which(egret$storage.type == "cold/wet")] <- "moist cold"
+egret$storage.type[which(egret$storage.type == "cold/moist")] <- "moist cold"
+
+#Dark
+egret$storage.type[which(egret$storage.type == "darkness")] <- "dark"
+egret$storage.type[which(egret$storage.type == "in darkness")] <- "dark"
 
 #TO CHECK:
 # natural as in outdoors?
@@ -168,6 +241,7 @@ egret$storage.type[which(egret$storage.type == "dry refrigerator")] <- "dry refr
 # controlled environment - not a room?
 # is wet and undried the same?
 # if just says dry or wet, should we assume at room temp?
+# Cold start?
 
 
 unique(egret$storage.humidity)
