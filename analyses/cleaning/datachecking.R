2d9600db91de9bb1b65fcf82f14978dc2c65b6ea
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2023-11-30 11:55:16 -0800

    data checking for methods

---
+++ analyses/cleaning/datachecking.R
@@ -0,0 +1,30 @@
+## Started 30 November 2023 ##
+## by Deirdre
+
+## Making notes of our methods --- have values summarizing the data we would like to reference
+
+## housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+## packages
+library(stringr)
+library(plyr)
+library(ggplot2)
+library(viridis)
+
+if(length(grep("deirdreloughnan", getwd()) > 0)) {
+  setwd("~/Documents/github/egret")
+} else if(length(grep("Lizzie", getwd()) > 0)) {
+  setwd("~/Documents/git/projects/others/deirdre/Synchrony")
+} else{
+  setwd("/Users/Lizzie/Documents/git/projects/egret/analyses") # for Lizzie
+}
+
+# Grab de data!
+d <- read.csv("analyses/output/egretData.csv")
+
+noStudy <- length(unique(d$datasetID))
+noSpp <- length(unique(d$sp.name))
+nRow <- nrow(d)
+
