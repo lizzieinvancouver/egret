f52ba3de97b0a7ae5ea74e652c1ba5e4f43320fc
FrederikBaumgarten <frederik.baumgarten@protonmail.ch>
2023-11-30 15:06:56 -0800

    update

--- analyses/cleaning/datachecking.R
+++ analyses/cleaning/datachecking.R
@@ -13,13 +13,13 @@ library(plyr)
 library(ggplot2)
 library(viridis)
 
-if(length(grep("deirdreloughnan", getwd()) > 0)) {
-  setwd("~/Documents/github/egret")
-} else if(length(grep("frederik", getwd()) > 0)) {
-  setwd("~/github/egret")
-} else{
-  setwd("/Users/Lizzie/Documents/git/projects/egret/analyses") # for Lizzie
-}
+#if(length(grep("deirdreloughnan", getwd()) > 0)) {
+#  setwd("~/Documents/github/egret")
+#} else if(length(grep("frederik", getwd()) > 0)) {
+#} else{
+#  setwd("/Users/Lizzie/Documents/git/projects/egret/analyses") # for Lizzie
+###  setwd("~/github/egret")
+#}
 
 # Grab de data!
 d <- read.csv("analyses/output/egretData.csv")
