37e7fa9262393d460e9535cb9d857bc41cd0929c
Xiaomao <wangxiaomao0209@gmail.com>
2024-02-24 10:19:44 -0800

    Species name corrected

--- analyses/cleaning/source/cleanspecies.R
+++ analyses/cleaning/source/cleanspecies.R
@@ -1,7 +1,11 @@
 ## Started 1 Dec 2023 ##
+## Revised 20 Feb 2024 by Mao##
 ## taken from cleaningDL.R ##
 
 # Needs to be sourced in cleanall.R
+
+library("taxize")
+library("stringr")
 
 # General chekcs:
 #1. fix typos and minor issues:
@@ -10,7 +14,7 @@ d$species <- tolower(d$species)
 d_species <- unique(paste(d$genus, d$species))
 
 # Use taxize package to inspect whether names are correct
-ref <- gnr_dasources() # Full list of dabases available
+ref <- gnr_datasources() # Full list of dabases available
 fix_names <- gnr_resolve(sci = d_species, with_canonical_ranks = T)
 d_species_fix <- unique(fix_names$matched_name2)
 names_changed <- setdiff(d_species, d_species_fix)
@@ -21,8 +25,10 @@ sort(d_species)
 # Remove trailing spaces:
 d$genus <- str_trim(d$genus)
 d$species <- str_trim(d$species)
+# Go back and run line 10-17 again
 
 # Fix#########################################
+
 d$species[which(d$genus == "Colutea" & d$species == "bohsei")] <- "buhsei"
 d$species[which(d$genus == "Abies" & d$species == "amabils")] <- "amabilis"
 d$species[which(d$genus == "Lathyrus" & d$species == "sativa")] <- "sativus"
@@ -37,7 +43,7 @@ d$species[which(d$genus == "Tradescantia" & d$species == "ohioensis")] <- "ohien
 d$species[which(d$genus == "Betula" & d$species == "albo-sinensis")] <- "albosinensis"
 
 d$genus[which(d$genus == "Pterocaryafra" & d$species == "fraxinifolia")] <- "Pterocarya"
-d$genus[which(d$genus == "Leontice\r\n" & d$species == "incerta")] <- "Leontice"
+#d$genus[which(d$genus == "Leontice\r\n" & d$species == "incerta")] <- "Leontice"
 d$genus[which(d$genus == "Aanigozanthos" & d$species == "flavidus")] <- "Anigozanthos"
 d$genus[which(d$genus == "Deginia" & d$species == "velebitica")] <- "Degenia"
 d$genus[which(d$genus == "Lingularia" & d$species == "sibirica")] <- "Ligularia"
@@ -46,17 +52,19 @@ d$genus[which(d$genus == "Eucalytpus" & d$species == "delegatensis")] <- "Eucaly
 d$genus[which(d$genus == "Jasminus" & d$species == "fruiticans")] <- "Jasminum"
 d$species[which(d$genus == "Jasminum" & d$species == "fruiticans")] <- "fruticans"
 
-d$variety[which(d$genus == "Pedicularis" & d$species == "longiflora var.\r\ntubiformis")] <- "tubiformis"
-d$species[which(d$genus == "Pedicularis" & d$species == "longiflora var.\r\ntubiformis")] <- "longiflora"
+d$variety[which(d$genus == "Pedicularis" & d$species == "longiflora var.\ntubiformis")] <- "tubiformis"
+d$species[which(d$genus == "Pedicularis" & d$species == "longiflora var.\ntubiformis")] <- "longiflora"
 d$species[which(d$genus == "Ferula" & d$species == "assa foetida")] <- "assa-foetida"
-d$species[which(d$genus == "Astrgalus" & d$species == "cyclophyllon")] <- "cyclophyllus"
-d$species[which(d$genus == "Astrgalus" & d$species == "cyclophyllu")] <- "cyclophyllus"
-d$species[which(d$species == "Amurensis")] <- "amurensis"
-d$species[which(d$species == "aviculare L.")] <- "aviculare"
-d$species[which(d$species == "longiflora var.\r\ntubiformis")] <- "longiflora"
-d$species[which(d$species == "Pagoda")] <- "pagoda"
-d$species[which(d$species == "Sylvestris L.")] <- "sylvestris"
-
+#d$species[which(d$genus == "Astrgalus" & d$species == "cyclophyllon")] <- "cyclophyllus"
+#d$species[which(d$genus == "Astrgalus" & d$species == "cyclophyllu")] <- "cyclophyllus"
+#d$species[which(d$species == "Amurensis")] <- "amurensis"
+#d$species[which(d$species == "aviculare L.")] <- "aviculare"
+#d$species[which(d$species == "longiflora var.\r\ntubiformis")] <- "longiflora"
+#d$species[which(d$species == "Pagoda")] <- "pagoda"
+#d$species[which(d$species == "Sylvestris L.")] <- "sylvestris"
+d$genus[which(d$genus == "Alstromeria" & d$species == "ligtu")] <- "Alstroemeria"
+d$species[which(d$genus == "Vitis" & d$species == "vinifera x amurensis")] <- "amurensis"
+d$species[which(d$genus == "Astragalus" & d$species == "cyclophyllu")] <- "cyclophyllos"
 # Confirm ####################################
 d_species <- unique(paste(d$genus, d$species))
 fix_names <- gnr_resolve(sci = d_species, with_canonical_ranks = T)
@@ -66,4 +74,5 @@ names_changed
 
 ### TO CHECK: look though the species names or there any that seem strange? If so---flag for discussion. For example:
 #Echinacea angustifolia, purpurea, pallida -- 3 species in 1 study
-
+#For this particular experiment, they mixed seeds of 3 species together.
+#Rows of NAs found in the dataset.
