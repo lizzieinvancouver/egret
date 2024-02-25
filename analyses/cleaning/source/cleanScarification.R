25783034df30e453b9799b67caa92816af9c360f
britanywuuu <britany1902@gmail.com>
2024-02-24 19:08:55 -0800

    finish general grouping

--- analyses/cleaning/source/cleanScarification.R
+++ analyses/cleaning/source/cleanScarification.R
@@ -1,64 +1,96 @@
-## Upded 30 January 2024 ##
-## By Deirdre ##
+## Updated 24 February 2024 ##
+## By Britany ##
 
-## This contains code to clean chill duration and temperature ##
+## This contains code to clean scarification and scarif type ##
 ## Original code taken from file called cleaningDL.R ##
-
+## 24 Feb subset each gen group, can start specific grouping ## 
 #1. Scarification
 unique(d$scarification)
 unique(d$scarif.type)
 
-# group specifically 
-d$scarifTypeA <- d$scarif.type
-
-d$scarifTypeA[which(d$scarifTypeA == "sandpaper")] <- "mechanical - sandpaper"
-d$scarifTypeA[which(d$scarifTypeA == "sand paper (Np. 150)")] <- "mechanical - sandpaper"
-
-d$scarifTypeA[which(d$scarifTypeA == "mechanical with razor")] <- "mechanical - razor"
-d$scarifTypeA[which(d$scarifTypeA == "H2SO4")] <- "chemical - H2SO4"
-
+# clean 
+# move hot to soaking and cold to cold trt
+d$scarifType <- d$scarif.type
 
 # group more generally --- chemical vs mechanical
-d$scarifTypeB <- d$scarif.type
+d$scarifTypeGen <- d$scarif.type
 # mechanical 
-d$scarifTypeB[which(d$scarifTypeB == "Mechanical")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "sandpaper")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "sand paper")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "sand paper (Np. 150)")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "mechanical - sandpaper")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "mechanical with sandpaper")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "mechanical - razor")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "mechanical with razor")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "coat removal")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "coat removal ")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "pericarp removal")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "tegument cutting")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "seed polishing")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "trimmed pappus")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "pelleted")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "seed coat removed")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "Seed coat removed")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "burned")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "scapel")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "scalpel on lenticular side")] <- "mechanical"
-d$scarifTypeB[which(d$scarifTypeB == "manual with needle at the cotyledon end.")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "Mechanical")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "sandpaper")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "sand paper")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "sand paper (Np. 150)")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "mechanical - sandpaper")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "mechanical with sandpaper")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "mechanical - razor")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "mechanical with razor")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "coat removal")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "coat removal ")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "pericarp removal")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "tegument cutting")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "seed polishing")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "trimmed pappus")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "pelleted")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "seed coat removed")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "Seed coat removed")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "burned")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "scapel")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "scalpel on lenticular side")] <- "mechanical"
+d$scarifTypeGen[which(d$scarifTypeGen == "manual with needle at the cotyledon end.")] <- "mechanical"
 
 # chemical
-d$scarifTypeB[which(d$scarifTypeB == "Chemical")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "H2SO4")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "H2SO4.98per.0min")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "H2SO4.98per.10min")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "H2SO4.98per.20min")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "H2SO4.98per.30min")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "acid scarification")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "acid")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "NaClO")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "3% H2O2 (2 hrs)")] <- "chemical"
-d$scarifTypeB[which(d$scarifTypeB == "chemical, H2O2")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "Chemical")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "H2SO4")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "H2SO4.98per.0min")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "H2SO4.98per.10min")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "H2SO4.98per.20min")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "H2SO4.98per.30min")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "acid scarification")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "acid")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "NaClO")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "3% H2O2 (2 hrs)")] <- "chemical"
+d$scarifTypeGen[which(d$scarifTypeGen == "chemical, H2O2")] <- "chemical"
 
-unique(d$scarifTypeB)
+
+# temperature
+d$scarifTypeGen[which(d$scarifTypeGen == "use temerpature to permeate seedcoat")] <- "temperature"
+d$scarifTypeGen[which(d$scarifTypeGen == "cold")] <- "temperature"
+
+
+# moisture 
+d$scarifTypeGen[which(d$scarifTypeGen == "soaking in water")] <- "moisture"
+d$scarifTypeGen[which(d$scarifTypeGen == "hot water scarification")] <- "moisture"
+d$scarifTypeGen[which(d$scarifTypeGen == "hot water 70C")] <- "moisture"
+d$scarifTypeGen[which(d$scarifTypeGen == "hot water")] <- "moisture"
+d$scarifTypeGen[which(d$scarifTypeGen == "hot.water.10min")] <- "moisture"
+d$scarifTypeGen[which(d$scarifTypeGen == "hot.water.20min")] <- "moisture"
+d$scarifTypeGen[which(d$scarifTypeGen == "hot.water.30min")] <- "moisture"
+
+# other
+unique(d$scarifTypeGen)
+
+#look at each general categories 
+d.temp <- subset(d,d$scarifTypeGen == "temperature")
+d.moisture <- subset(d,d$scarifTypeGen == "moisture") #move water to soaking column??
+d.mechanical <- subset(d,d$scarifTypeGen == "mechanical")
+d.chemical <- subset(d,d$scarifTypeGen == "chemical")
+unique(d.temp$datasetID)
+unique(d.moisture$datasetID)
+unique(d.mechanical$datasetID)
+unique(d.chemical$datasetID)
+
+# group specifically 
+d$scarifTypeSpe <- d$scarif.type
+
+d$scarifTypeSpe[which(d$scarifTypeSpe == "sandpaper")] <- "mechanical - sandpaper"
+d$scarifTypeSpe[which(d$scarifTypeSpe == "sand paper (Np. 150)")] <- "mechanical - sandpaper"
+
+d$scarifTypeSpe[which(d$scarifTypeSpe == "mechanical with razor")] <- "mechanical - razor"
+d$scarifTypeSpe[which(d$scarifTypeSpe == "H2SO4")] <- "chemical - H2SO4"
+
+
 ### Check unusual entries--- 
-### Y, cold, seed coat, partly scarified, use temerpature to permeate seedcoat,
+### Y, cold, seed coat, partly scarified, use temperature to permeate seedcoat,
 ### partial scarification of seed coat, soaking in water
 # soaking---wrong column? is this da in the soaking column?
+# remove soaking, check if data for the whole paper is good, if not take it to lab meeting
 # is hot water scarification vs soaking---to dicuss as a group
