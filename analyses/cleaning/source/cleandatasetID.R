03011326b767df9cd461371bba8f1360bdc4030a
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2024-02-29 21:45:46 -0800

    worked on cleaning datasetid

--- analyses/cleaning/source/cleandatasetID.R
+++ analyses/cleaning/source/cleandatasetID.R
@@ -14,9 +14,7 @@ temp <- subset(subby, dup != "FALSE")
 
 subby$dup <- duplicated(subby[,c("studyID")])
 temp <- subset(subby, dup != "FALSE") 
-
-#But how many of these are actually scraped?
-dupID <- unique(temp$studyID)
+# Most of these were not actually scraped!
 
 # Fix ones with incorrect years
 d$datasetID[which(d$datasetID == "acosta12")] <- "acosta13"
@@ -30,15 +28,16 @@ d$datasetID[which(d$datasetID == "picciau18")] <- "picciau19"
 d$datasetID[which(d$datasetID == "tang10a")] <- "tang10_1"
 d$datasetID[which(d$datasetID == "tang10b")] <- "tang10_2"
 
-# Find datasetID's that are duplicated---ie a author published 2 papers in the same year: change to authorYeara and authorYearb
-#cho18: gg and HHN
+d$datasetID[which(d$datasetID == "Aldridge1993")] <- "Aldridge1992"
+d$datasetID[which(d$datasetID == "Aldridge1994")] <- "Aldridge1992"
+d$datasetID[which(d$datasetID == "Aldridge1995")] <- "Aldridge1992"
+d$datasetID[which(d$datasetID == "Aldridge1996")] <- "Aldridge1992"
+d$datasetID[which(d$datasetID == "Aldridge1997")] <- "Aldridge1992"
 
-#gremer20 DM scraped one only---why?
-#li11 - HHN, julie
-#Ren08 --- same DM
-#tang10 MN
-#Yan16 MN
-#Yang20 ---MN
-#yang16-- me and mn
-#zhang21-- JN
+View(unique(d$datasetID))
 
+Chen06
+Chen15
+chichizola18
+
+# irvani12,13,14; Kolodziejek18,19, li11 with space; Meyer94,95; Olmez07-09; Rouhi12,13; Sacande04,05; Tilki06,07; tylkowski09-10; NA?
