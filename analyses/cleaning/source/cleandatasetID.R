7573697c0e9a4081878244e6887662a8a2544fb9
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2024-03-04 16:43:20 -0800

    worked on cleaning datasetID

--- analyses/cleaning/source/cleandatasetID.R
+++ analyses/cleaning/source/cleandatasetID.R
@@ -28,11 +28,18 @@ d$datasetID[which(d$datasetID == "picciau18")] <- "picciau19"
 d$datasetID[which(d$datasetID == "tang10a")] <- "tang10_1"
 d$datasetID[which(d$datasetID == "tang10b")] <- "tang10_2"
 
+d$datasetID[which(d$datasetID == "li11 ")] <- "li11"
+
 d$datasetID[which(d$datasetID == "Aldridge1993")] <- "Aldridge1992"
 d$datasetID[which(d$datasetID == "Aldridge1994")] <- "Aldridge1992"
 d$datasetID[which(d$datasetID == "Aldridge1995")] <- "Aldridge1992"
 d$datasetID[which(d$datasetID == "Aldridge1996")] <- "Aldridge1992"
 d$datasetID[which(d$datasetID == "Aldridge1997")] <- "Aldridge1992"
+
+d$datasetID[which(d$datasetID == "irvani13")] <- "irvani12"
+d$datasetID[which(d$datasetID == "irvani14")] <- "irvani12"
+
+d$datasetID[which(d$datasetID == "Sacande05")] <- "Sacande04"
 
 View(unique(d$datasetID))
 
@@ -40,4 +47,5 @@ Chen06
 Chen15
 chichizola18
 
-# irvani12,13,14; Kolodziejek18,19, li11 with space; Meyer94,95; Olmez07-09; Rouhi12,13; Sacande04,05; Tilki06,07; tylkowski09-10; NA?
+
+# Checked, these are correct: Kolodziejek18,19, Meyer94,95; Olmez07-09; Rouhi12,13; Tilki06,07; tylkowski09-10
