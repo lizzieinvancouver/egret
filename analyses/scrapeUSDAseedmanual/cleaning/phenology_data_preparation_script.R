5266548683e91665392839cd72ae518140ad53e0
Selena Shew <shewselena@gmail.com>
2024-06-18 17:18:58 -0700

    updated germination spreadsheet & added phenology test data

--- analyses/scrapeUSDAseedmanual/cleaning/phenology_data_preparation_script.R
+++ analyses/scrapeUSDAseedmanual/cleaning/phenology_data_preparation_script.R
@@ -39,37 +39,6 @@ prep_phen_data <- function(directory) {
       data <- data[seq_len(empty_rows[1] - 1), ]
     }
     
-    # Remove quotation marks from all cells
-    data <- data %>% mutate(across(everything(), ~ str_replace_all(.x, "'", "")))
-    
-    # Find the row index where the header contains "Table"
-    table_row <- which(str_detect(data[[1]], "Table"))
-    
-    if (length(table_row) > 0) {
-      # Get the header row as character vector
-      header_row <- as.character(data[table_row, ])
-      
-      # Extract pdf_table_number between "Table " and "-"
-      pdf_table_number <- str_extract(header_row, "(?<=Table )[0-9]+")
-      
-      # Repeat pdf_table_number for each row in the dataframe
-      pdf_table_number <- rep(pdf_table_number, nrow(data))
-      
-      # Extract genus_name between "Table [number]-" and ","
-      genus_name <- str_extract(header_row, "(?<=Table [0-9]+-)(.*?)(?=,|$)")
-      
-      # Repeat genus_name for each row in the dataframe
-      genus_name <- rep(genus_name, nrow(data))
-      
-      # Add pdf_table_number and genus_name as new columns
-      data <- data %>% 
-        mutate(pdf_table_number = pdf_table_number,
-               genus_name = genus_name)
-    } else {
-      # If "Table" header is not found, set pdf_table_number and genus_name to NA
-      data$pdf_table_number <- NA
-      data$genus_name <- NA
-    }
     
     # Write the modified data back to the CSV file
     write_csv(data, file_path)
