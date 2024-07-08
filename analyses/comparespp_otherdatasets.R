10b164bf11621d73b3ea5c9ecb7be8d37349870c
wangxm-forest <wangxiaomao0209@gmail.com>
2024-07-08 15:29:36 -0700

    Calculated overlaps between different datasets

--- analyses/comparespp_otherdatasets.R
+++ analyses/comparespp_otherdatasets.R
@@ -1,7 +1,7 @@
 # Started May 3, 2022 by deirde
 # Updates in November 2023 by Lizzie
-
-# Compare the EGRET data to other datasets (currently Baskin^2 and OSPREE)
+# Updates in July 2024 by Mao
+# Compare the EGRET data to other datasets (currently Baskin^2 and OSPREE, and USDA)
 
 # housekeeping
 rm(list=ls()) 
@@ -15,48 +15,69 @@ if(length(grep("deirdreloughnan", getwd()) > 0)) {
   setwd("~/Documents/github/egret/analyses")
 } else if(length(grep("lizzie", getwd()) > 0)) {
   setwd("/Users/lizzie/Documents/git/projects/egret/analyses")
+} else if(length(grep("Xiaomao", getwd()) > 0)) {
+  setwd("C:/PhD/Project/egret/analyses")
 } else{
   setwd("boomdittyboom") # for midge
 }
 
 # Get the datasets
-egret <- read.csv("input/egretData.csv")
+egret <- read.csv("output/egretclean.csv")
 egret$latbi <- paste(egret$genus, egret$species, sep = "_")
 
 ospree <- read.csv("input/ospree_clean_withchill.csv")
 ospree$latbi <- paste(ospree$genus, ospree$species, sep = "_")
 
-# Merge with OSPREE 
-egret$latbi <- str_trim(egret$latbi)
-ospree$latbi  <- str_trim(ospree$latbi)
-ospreeSp <- unique(ospree$latbi)
-egretSp <- unique(egret$latbi)
-egretSub <- egret[egret$latbi %in% ospreeSp,]
+USDA <- read.csv("scrapeUSDAseedmanual/output/usdaGerminationData.csv")
+USDA$latbi <- paste(USDA$genus, USDA$species, sep = "_")
 
-sort(unique(egretSub$latbi))
-spInterest <- c("Fagus_sylvatica", "Betula_pendula", "Carpinus_betulus", "Picea_abies", "Picea_glauca",
-  "Liquidambar_styracilua", "Prunus_avium","Pinus_sylvestris")
-egretStudy <- egretSub[egretSub$latbi %in% spInterest,] 
-unique(egretStudy$datasetID) 
-
-write.csv(egretStudy, "output/ospree8studies.csv", row.names = FALSE)
-
-# Merge with BaskinSquared
 bb <- read.csv("input/Baskin_Dormancy_Database.csv", skip=2) 
 bb$X1 <- NULL
-baskinsp <- unique(bb$Genus_species)
+bb$latbi <- bb$Genus_species
 
-# FYI, there is more than 1 row per sp
-dim(bb)
-length(baskinsp)
+egret$latbi <- str_trim(egret$latbi)
+ospree$latbi  <- str_trim(ospree$latbi)
+USDA$latbi <- str_trim(USDA$latbi)
+bb$latbi <- str_trim(bb$latbi)
 
-ospbask <- bb[which(bb$Genus_species %in% ospreeSp),]
-egrbask <- bb[which(bb$Genus_species %in% egretSp),]
+# Compare OSPREE x EGRET
+intersect_eo <- intersect(egret$latbi, ospree$latbi)
+length(intersect_eo)# 19 sp
 
-length(egretSp)
-dim(egrbask)
+# Compare OSPREE x USDA
+intersect_ou <- intersect(ospree$latbi, USDA$latbi)
+length(intersect_ou)# 60 sp
 
-table(ospbask$Dormancy.Class)
-table(egrbask$Dormancy.Class)
+# Compare EGRET x USDA (for fun!)
+intersect_eu <- intersect(egret$latbi, USDA$latbi)
+length(intersect_eu)# 17 sp
 
-subset(ospbask, Dormancy.Class=="ND")
+# Compare Baskin x EGRET
+intersect_be <- intersect(bb$latbi, egret$latbi)
+length(intersect_be)# 198 sp
+
+# Compare Baskin x USDA
+intersect_bu <- intersect(bb$latbi, USDA$latbi)
+length(intersect_bu)# 199 sp
+
+# Combine egret and USDA
+egretusda <- data.frame(latbi = c(egret$latbi, USDA$latbi))
+
+# Compare OSPREE x (EGRET + USDA)
+intersect_o_eu <- intersect(ospree$latbi, egretusda$latbi)
+length(intersect_o_eu)# 71 sp
+
+# Compare Baskin x (EGRET + USDA)
+intersect_b_eu <- intersect(bb$latbi, egretusda$latbi)
+length(intersect_b_eu)# 381 sp
+
+# Egret x Ospree x USDA x Baskin
+ospreeSp <- unique(ospree$latbi)
+egretSp <- unique(egret$latbi)
+usdaSp <- unique(USDA$latbi)
+bbSp <- unique(bb$latbi)
+egretSub <- egret[egret$latbi %in% ospreeSp,]
+egretSub <- egretSub[egretSub$latbi %in% usdaSp,]
+egretSub <- egretSub[egretSub$latbi %in% bbSp,]
+
+sort(unique(egretSub$latbi))# 8 sp
