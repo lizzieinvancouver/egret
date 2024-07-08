db011841221dfa4511a8b19f8d565dc9c82f18d2
wangxm-forest <wangxiaomao0209@gmail.com>
2024-07-08 11:46:13 -0700

    Editted dormancyexplor.R

--- analyses/dormancyexplor.R
+++ analyses/dormancyexplor.R
@@ -6,10 +6,14 @@ graphics.off()
 
 library(dplyr)
 
-setwd(dir = "~/Documents/git/egret/analyses/output/")
+if(length(grep("dan?#you can change this later", getwd()) > 0)) {
+  setwd(dir = "~/Documents/git/egret/analyses/output/")
+} else if(length(grep("Xiaomao", getwd()) > 0)) {
+  setwd("C:/PhD/Project/egret/analyses")
+}
 
-d<-read.csv("egretclean.csv")
-bask<-read.csv("..//input/Baskin_Dormancy_Database.csv")
+d<-read.csv("output/egretclean.csv")
+bask<-read.csv("input/Baskin_Dormancy_Database.csv")
 
 colnames(bask)<-bask[2,]
 bask<-bask[3:14256,]
@@ -27,6 +31,30 @@ d.perc<-filter(d, respvar %in% c("per.germ"))
 makefin<-d.perc %>% group_by(datasetID,study,Genus_species) %>% slice(which.max(response))
 
 counter<-d %>% group_by(`Dormancy Class`, Genus_species) %>% count()
+
+#Counting how many species are there in every dormancy class
+d.perc$DormancyClass <- as.factor(d.perc$`Dormancy Class`)
+d.perc$scarification <- as.factor(d.perc$scarification)
+dormclass <- aggregate(Genus_species ~ DormancyClass, data = d.perc, FUN = function(x) length(unique(x)))
+
+print(dormclass)
+#MPD-22
+#ND-22
+#PD-142
+#PY-8
+#PYPD-2
+
+#Subset every dormancy class
+MPD <- d.perc[d.perc$DormancyClass == "MPD", ]
+summary(MPD$scarification)
+ND <- d.perc[d.perc$DormancyClass == "ND", ]
+summary(ND$scarification)
+PD <- d.perc[d.perc$DormancyClass == "PD", ]
+summary(PD$scarification)
+PY <- d.perc[d.perc$DormancyClass == "PY", ]
+summary(PY$scarification)
+PYPD <- d.perc[d.perc$DormancyClass == "PYPD", ]
+summary(PYPD$scarification)
 
 library(ggplot2)
 ggplot(d.perc,aes(scarification,response))+geom_boxplot(aes(color=`Dormancy Class`,group=`Dormancy Class`))
