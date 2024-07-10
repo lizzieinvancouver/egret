bdf58edd55a711289edac0d6d46452b33e21f81f
wangxm-forest <wangxiaomao0209@gmail.com>
2024-07-10 13:54:59 -0700

    Editted sp overlapping

--- analyses/comparespp_otherdatasets.R
+++ analyses/comparespp_otherdatasets.R
@@ -39,35 +39,41 @@ ospree$latbi  <- str_trim(ospree$latbi)
 USDA$latbi <- str_trim(USDA$latbi)
 bb$latbi <- str_trim(bb$latbi)
 
+ospreeSp <- unique(ospree$latbi)
+egretSp <- unique(egret$latbi)
+usdaSp <- unique(USDA$latbi)
+bbSp <- unique(bb$latbi)
+
+
 # Compare OSPREE x EGRET
-intersect_eo <- intersect(egret$latbi, ospree$latbi)
+intersect_eo <- intersect(egretSp, ospreeSp)
 length(intersect_eo)# 19 sp
 
 # Compare OSPREE x USDA
-intersect_ou <- intersect(ospree$latbi, USDA$latbi)
+intersect_ou <- intersect(ospreeSp, usdaSp)
 length(intersect_ou)# 60 sp
 
 # Compare EGRET x USDA (for fun!)
-intersect_eu <- intersect(egret$latbi, USDA$latbi)
+intersect_eu <- intersect(egretSp, usdaSp)
 length(intersect_eu)# 17 sp
 
 # Compare Baskin x EGRET
-intersect_be <- intersect(bb$latbi, egret$latbi)
+intersect_be <- intersect(bbSp, egretSp)
 length(intersect_be)# 201 sp
 
 # Compare Baskin x USDA
-intersect_bu <- intersect(bb$latbi, USDA$latbi)
+intersect_bu <- intersect(bbSp, usdaSp)
 length(intersect_bu)# 202 sp
 
 # Combine egret and USDA
-egretusda <- data.frame(latbi = c(egret$latbi, USDA$latbi))
+egretusda <- data.frame(latbi = c(egretSp, usdaSp))
 
 # Compare OSPREE x (EGRET + USDA)
-intersect_o_eu <- intersect(ospree$latbi, egretusda$latbi)
+intersect_o_eu <- intersect(ospreeSp, egretusda$latbi)
 length(intersect_o_eu)# 71 sp
 
 # Compare Baskin x (EGRET + USDA)
-intersect_b_eu <- intersect(bb$latbi, egretusda$latbi)
+intersect_b_eu <- intersect(bbSp, egretusda$latbi)
 length(intersect_b_eu)# 387 sp
 
 # Egret x Ospree x USDA x Baskin
