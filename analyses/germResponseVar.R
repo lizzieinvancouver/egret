bc810ba722748cb145c4cb056e2bd9e71cd4d7e1
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2024-07-10 08:49:31 -0700

    cal final percent germ

--- analyses/germResponseVar.R
+++ analyses/germResponseVar.R
@@ -23,6 +23,9 @@ if(length(grep("deirdreloughnan", getwd()) > 0)) {
 
 d <- read.csv("output/egretclean.csv")
 
+# forcing a few things to be numberic:
+d$germDuration <- as.numeric(d$germDuration)
+
 chilled <- subset(d, !is.na(chill.tempCor)) # chilling temps for 17141 rows or 213 studies
 
 chillGerm <- subset(chilled, !is.na(chilled$germ.tempCor)) # chilled with germ temps 16500
@@ -44,9 +47,8 @@ length(unique(tempyyy$datasetID))
 # rows of data, number study (experiments per paper), papers, species
 # how many of these are time series, how many of the mgt species are also in the percent germ
 
-d$sp.name <- paste(d$genus, d$species, sep = "_")
+d$spName <- paste(d$genus, d$species, sep = "_")
 d$studyExp <- as.factor(paste(d$datasetID, d$study, sep = " "))
-
 
 responses <- c("percent.germ", "prop.germ", "germ.rate", "mgt", "50%.germ", "germ.time")
 
@@ -57,7 +59,7 @@ temp <- subset(d, responseVar == responses[i])
 germResp[1,i] <- round((nrow(temp)/nrow(d))*100,0)
 germResp[2,i] <- length(unique(temp$datasetID)) # 19
 germResp[3,i]<- length(unique(temp$studyExp)) # 25
-germResp[4,i]<- length(unique(temp$sp.name)) # 23
+germResp[4,i]<- length(unique(temp$spName)) # 23
 }
 names(germResp) <-  c("percent.germ", "prop.germ", "germ.rate", "mgt", "50%.germ", "germ.time")
 germResp
@@ -65,7 +67,73 @@ germResp
 # script plot time series; how many 
 #how many of the mgt species are also in the percent germ
 mgt <- subset(d, responseVar == "mgt")
-mgtSp <- unique(mgt$sp.name)
+mgtSp <- unique(mgt$spName)
 
 perGerm <- subset(d, responseVar == "percent.germ")
-temp <- perGerm[perGerm$sp.name %in% mgtSp, ]; length(unique(temp$sp.name))
+temp <- perGerm[perGerm$spName %in% mgtSp, ]; length(unique(temp$spName))
+
+# How much of the data in the percent germination is time series data?
+
+curved <- unique(perGerm[, c("datasetID", "spName", "germDuration")])
+curved$count <- 1
+noDurations <- aggregate(curved["count"], curved[c("datasetID", "spName")], FUN = sum)
+curvey <- subset(noDurations, count > 2) 
+length(unique(curvey$datasetID)) #90
+
+curveStudy <- sort(unique(curvey$datasetID))
+dCurve <- d[d$datasetID %in% curveStudy, ]
+
+length(unique(dCurve$spName)) #132
+
+for(i in 1:length(curveStudy)){
+   pdf(paste("figures/timeSeriesCurves/", curveStudy[i], ".pdf"), width = 5, height = 5)
+  #i <- 1
+   temp <- subset(dCurve, datasetID == curveStudy[i])
+   
+   plot(temp$responseValueNum ~ temp$germDuration, type = 'n', main = curveStudy[i],
+     xlim = c(0,200))
+   
+   for(j in 1:length(unique(temp$spName))){
+    # j <- 2
+     sp <- unique(temp$spName)
+     tempy <- subset(temp, spName == sp[j])
+   points(tempy$responseValueNum ~ tempy$germDuration, type = "l")
+   
+   }
+   dev.off()
+}
+
+# Want to extract the last % germ for every species and the maximum length of time, but account for treatment differences
+
+pgSub <- perGerm[,c("datasetID", "study", "spName","chill.tempCor", "chill.durationCor", "germDuration", "germTemp", "scarifType", "chemicalCor", "responseValueNum")]
+
+
+perGerm$study[which(is.na(perGerm$study))] <- "exp1"
+
+pgSub$dataExp <- paste(perGerm$datasetID, perGerm$study, perGerm$spName, sep = "_")
+pgSub$trt <- paste(perGerm$chill.durationCor, perGerm$chill.tempCor, perGerm$germTemp, perGerm$chemicalCor, perGerm$scarifType, sep = "_")
+
+#pgSub$germTrt <- paste(perGerm$germDuration, perGerm$germTemp, sep = "_")
+
+perGermSub <- subset(pgSub, trt != "NA_NA_NA_NA_NA")
+#temp <- aggregate(pgSub[c("responseValueNum")], pgSub[c("datasetID", "study","spName","chillTrt", "germTrt", "scarifType", "chemicalCor")], FUN = max, na.rm = T)
+
+studyID <- sort(unique(pgSub$datasetID))
+dataExp <- sort(unique(pgSub$dataExp))
+trtVar <- sort(unique(pgSub$trt))
+
+length(unique(pgSub$datasetID)) #234 study
+
+singlePerG <- vector()
+for(i in 1:length(studyID)){
+  temp <- subset(pgSub, datasetID == studyID[i])
+  trtVar <- unique(temp$trt)
+  for(j in 1:length(trtVar)){
+    tempV <- subset(temp, trt == trtVar[j])
+    tempPG <- aggregate(tempV[c("responseValueNum", "germDuration")], tempV[c("trt", "spName", "dataExp")], FUN = max)
+    singlePerG <- rbind(singlePerG, tempPG)
+  }
+}
+
+#Each species within a paper, with each line being a treatment
+
