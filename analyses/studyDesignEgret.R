8a81d410028e4d3a54f7eeaf4e7be9630106bab0
Elizabeth M Wolkovich <lizziemetier@gmail.com>
2024-07-25 18:45:40 -0700

    Small progress on egret study design

--- analyses/studyDesignEgret.R
+++ analyses/studyDesignEgret.R
@@ -1,24 +1,23 @@
 #=== === === === === === === === === === === === === === === === === === === ===
-# created by Lizzie and Christophe on July 8, 2024
+# created by Lizzie and Christophe on 8 July 2024
 # cleaning treatment column of egret
 
 # housekeeping
 rm(list=ls())  
 options(stringsAsFactors=FALSE)
-library(dplyr)
 
 if(length(grep("christophe_rouleau-desrochers", getwd()) > 0)) {
   setwd("~/Documents/github/egret/analyses")
 } else if(length(grep("danielbuonaiuto", getwd()) > 0)) {
   setwd("/Users/danielbuonaiuto/Documents/git/egret/analyses/")
 } else if(length(grep("lizzie", getwd()) > 0)) {
-  setwd("/Users/christophe_rouleau-desrochers/Documents/github/egret/analyses")
+  setwd("/Users/lizzie/Documents/git/projects/egret/analyses")
 }
 
 # read file
 d <- read.csv("output/egretclean.csv", sep=",", stringsAsFactors = FALSE)
 
-# #Create warm strat column
+# #Create warm strat column (this code now in analyseSeedCues/dropExtraTreats.R and I think could be DELETED?)
 d$warmstrat <- NA
 # vector for all treatments that have warm stratification
 warmstrat.names <- unique(d$treatment[grep("warm", d$treatment)])
@@ -26,14 +25,65 @@ warmstrat.names <- unique(d$treatment[grep("warm", d$treatment)])
 warmstrat.names[!warmstrat.names %in% c("cold strat + soak in warm water")]
 # add a 1 in warmstrat column whenever "warm" appepeared in the treatment column
 d$warmstrat[which(d$treatment %in% warmstrat.names)] <- 1
-
-
-d$provLatLon <- paste(d$provenance.lat,d$provenance.long ,sep=" ")
-# create a vector of columns to check
-col2check <- c("germTemp", "chillTemp","chillDuration", "warmstrat", "scarifType", "chemicalCor", "storageType","provLatLon")
 ##add provinence
+d$provLatLon <- paste(d$provenance.lat,d$provenance.long ,sep=" ")
 
-##############Dan's (unnecessary) dplyr solution##########################################
+
+
+##
+## Count up unique treatments for a TON of the columns
+# START HERE ... add species, count NA and non-NA separately, then do what columns happen TOGETHER
+
+# keep only one datasetIDstudy
+d.unique <- d[!duplicated(d$datasetIDstudy), ]
+# create a vector of datasetIDstudy
+unique.studies <- d.unique$datasetIDstudy
+# create a vector of columns to check
+col2check <- c("germTemp", "chillTemp","chillDuration", "warmstrat", "scarifType", 
+  "chemicalCor", "storageType","provLatLon")
+
+# creating an empty data frame in a few steps to match col2check
+# vector that will be the new df columns 
+colsforstudydesign <- c("datasetIDstudy", col2check)
+# fill the right ncol and nrow
+studydesign <- data.frame(matrix(ncol = length(colsforstudydesign), nrow=length(unique.studies)))
+# set colnames
+names(studydesign) <- colsforstudydesign
+# add datasetIDstudy names in the first columns
+studydesign$datasetIDstudy <- unique.studies
+
+# loop to count unique treatments in each column
+# currently treats NAs as a treatment (NAs are not deleted out)
+
+for (i in c(1:length(unique.studies))) { # i = 1
+  subby <- d[which(d$datasetIDstudy == unique.studies[i]),]
+  for(j in c(1:length(col2check))) { # j = 2
+    unique.rows <- length(unique(subby[, c(col2check[j])]))
+    studydesign[i, j +1 ] <- unique.rows
+  }
+} 
+
+
+# double checking the entries 
+# check work above for a random selection of studies
+studiestocheck <- c("ochuodho08exp3", "liu13exp2", "downie98exp1")
+for(studyhere in studiestocheck){
+  dfhere <- subset(d, datasetIDstudy==studyhere)
+  ### germ.temp
+  print(length(unique(dfhere$germTemp)))
+  # germ duration
+  print(length(unique(dfhere$germDuration)))
+  # chemicalCor
+  print(length(unique(dfhere$chemicalCor)))
+  print(subset(studydesign, datasetIDstudy == studyhere))
+}
+
+
+
+if(FALSE){
+##
+## Dan's dplyr solution
+library(dplyr)
 
 treters<-dplyr::select(d,datasetID,study,latbi)
 treters<-distinct(treters)
@@ -49,10 +99,7 @@ treters<-dplyr::left_join(treters,goo)
 }
 
 
-
-
 treat<-tidyr::gather(treters,"treatment","n",4:11)
-
 write.csv(treat,"output/treatments_manipulated.csv")
 
 
@@ -83,55 +130,11 @@ goober<-dplyr::filter(goober,n>1)
 d.germ<-dplyr::filter(d.germ, datasetIDstudy %in% goober$datasetIDstudy)
 library(ggplot2)
 ggplot(d.germ,aes(tempDay,response))+geom_point(aes(color=latbi))+facet_wrap(~datasetIDstudy)
-
-
-
-#####################################################################################
-
 library(xtable)
 xtable(treters)
-
-    
-
-
-# keep only one datasetIDstudy
-d.unique <- d[!duplicated(d$datasetIDstudy), ]
-# create a vector of datasetIDstudy
-unique.studies <- d.unique$datasetIDstudy # paste speces
-
-
-# create an empty data frame
-
-# vector that will be the new df columns 
-colsforstudydesign <- c("datasetIDstudy", col2check)
-# fill the right ncol and nrow
-studydesign <- data.frame(matrix(ncol = length(colsforstudydesign), nrow=length(unique.studies)))
-# set colnames
-names(studydesign) <- colsforstudydesign
-# add datasetIDstudy names in the first columns
-studydesign$datasetIDstudy <- unique.studies
-
-# loop to count unique treatments in each column
-# currently treats NAs as a treatment (NAs are not deleted out)
-
-for (i in c(1:length(unique.studies))) { # i = 1
-  subby <- d[which(d$datasetIDstudy == unique.studies[i]),]
-  for(j in c(1:length(col2check))) { # j = 2
-    unique.rows <- length(unique(subby[, c(col2check[j])]))
-    studydesign[i, j +1 ] <- unique.rows
-  }
-} 
-
-# double checking the entries 
-# check work above for a random selection of studies
-studiestocheck <- c("ochuodho08exp3", "liu13exp2", "downie98exp1")
-for(studyhere in studiestocheck){
-  dfhere <- subset(d, datasetIDstudy==studyhere)
-  ### germ.temp
-  print(length(unique(dfhere$germTemp)))
-  # germ duration
-  print(length(unique(dfhere$germDuration)))
-  # chemicalCor
-  print(length(unique(dfhere$chemicalCor)))
-  print(subset(studydesign, datasetIDstudy == studyhere))
 }
+## End of Dan's code for now ....
+##
+
+
+
