debef28122fddad52c375c31417e1a399f4a8fda
FrederikBaumgarten <frederik.baumgarten@protonmail.ch>
2024-08-15 21:30:04 +0200

    update code with expand.grid function

--- analyses/treatComboUSDA.R
+++ analyses/treatComboUSDA.R
@@ -50,7 +50,7 @@ egret$scarifTypeGen
 
 unique(egret$latbi)
 
-length(unique(egret$spec))
+length(unique(egret$latbi))
 
 
 unique(egret$photoperiod)
@@ -62,7 +62,7 @@ egret$germ.tempCor
 str(egret)
 
 #how many species are in each datasetID
-datasetID <- aggregate(species ~ datasetID, data = egret, FUN = function(x) length(unique(x)))
+datasetID <- aggregate(latbi ~ datasetID, data = egret, FUN = function(x) length(unique(x)))
 
 
 #################################################################################
@@ -84,14 +84,26 @@ usda$chill.duraton<-ifelse(!is.na(usda$cold.stratification.duration),usda$cold.s
 
 #
 usda$responseVarClean <- usda$responseVar
+
+unique(usda$responseVarClean)
+
+#how many uniques entries are in usda$responseVar
+
+tapply(usda$speciesID, usda$responseVarClean, function(x) length(!is.na(x)))
+aggregate(speciesID ~ responseVar, data = usda, FUN = function(x) length(unique(x)))
+summary(usda$responseVar)
+names(usda)
+usda$responseVarClean
+usda[usda$responseVarClean == "percent.germ","responseVarClean"]
+
 #these columns show probably the same type of percentage, so rename them to "perc.standard"
-usda$responseVar_clean[usda$responseVar_clean == "percent.germ"] <- "perc.standard"
-usda$responseVar_clean[usda$responseVar_clean == "percent.germ.total"] <- "perc.standard"
-usda$responseVar_clean[usda$responseVar_clean == "germ.capacity"] <- "perc.standard"
-usda$responseVar_clean[usda$responseVar_clean == "mean.germ.capacity"] <- "perc.standard"
-usda$responseVar_clean[usda$responseVar_clean == "percent.germ.15degC.incubated"] <- "perc.standard"
+usda$responseVarClean[usda$responseVarClean == "percent.germ"] <- "perc.standard"
+usda$responseVarClean[usda$responseVarClean == "percent.germ.total"] <- "perc.standard"
+usda$responseVarClean[usda$responseVarClean == "germ.capacity"] <- "perc.standard"
+usda$responseVarClean[usda$responseVarClean == "mean.germ.capacity"] <- "perc.standard"
+usda$responseVarClean[usda$responseVarClean == "percent.germ.15degC.incubated"] <- "perc.standard"
 #another two that are probably the same
-usda$responseVar_clean[usda$responseVar_clean == "mean.percent.germ.energy"] <- "percent.germ.energy"
+usda$responseVarClean[usda$responseVarClean == "mean.percent.germ.energy"] <- "percent.germ.energy"
 
 #make a new column "spec" to combine genus and species
 usda$latbi <- paste(usda$genus, usda$species, sep = "_")
@@ -156,22 +168,65 @@ usda$scar_indic[!is.na(usda$scarifTypeGen)] <- "yes"
 #how many rows are there with "forc_indic" == "yes"
 length(usda$forc_indic[usda$forc_indic == "yes"]) 
 #how many unique species with "forc_indic" == "yes"
-length(unique(usda$spec[usda$forc_indic == "yes"])) #270 species
+length(unique(usda$latbi[usda$forc_indic == "yes"])) #270 species
+
+
+
 
 # Save each value for every combination
 combination_table <- data.frame(forc_indic = c("yes", "yes", "yes", "yes", "no", "no", "no", "no"),
                                 chill_indic = c("yes", "yes", "no", "no", "yes", "yes", "no", "no"),
                                 scar_indic = c("yes", "no", "yes", "no", "yes", "no", "yes", "no"),
-                                number = c(length(unique(usda$spec[usda$forc_indic == "yes" & usda$chill_indic == "yes" & usda$scar_indic == "yes"])),
-                                           length(unique(usda$spec[usda$forc_indic == "yes" & usda$chill_indic == "yes" & usda$scar_indic == "no"])),
-                                           length(unique(usda$spec[usda$forc_indic == "yes" & usda$chill_indic == "no" & usda$scar_indic == "yes"])),
-                                           length(unique(usda$spec[usda$forc_indic == "yes" & usda$chill_indic == "no" & usda$scar_indic == "no"])),
-                                           length(unique(usda$spec[usda$forc_indic == "no" & usda$chill_indic == "yes" & usda$scar_indic == "yes"])),
-                                           length(unique(usda$spec[usda$forc_indic == "no" & usda$chill_indic == "yes" & usda$scar_indic == "no"])),
-                                           length(unique(usda$spec[usda$forc_indic == "no" & usda$chill_indic == "no" & usda$scar_indic == "yes"])),
-                                           length(unique(usda$spec[usda$forc_indic == "no" & usda$chill_indic == "no" & usda$scar_indic == "no"]))))
+                                number = c(length(unique(usda$latbi[usda$forc_indic == "yes" & usda$chill_indic == "yes" & usda$scar_indic == "yes"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "yes" & usda$chill_indic == "yes" & usda$scar_indic == "no"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "yes" & usda$chill_indic == "no" & usda$scar_indic == "yes"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "yes" & usda$chill_indic == "no" & usda$scar_indic == "no"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "no" & usda$chill_indic == "yes" & usda$scar_indic == "yes"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "no" & usda$chill_indic == "yes" & usda$scar_indic == "no"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "no" & usda$chill_indic == "no" & usda$scar_indic == "yes"])),
+                                           length(unique(usda$latbi[usda$forc_indic == "no" & usda$chill_indic == "no" & usda$scar_indic == "no"]))))
 
 combination_table
+
+##### same thing using expand.grid
+# Create a data frame with all combinations of forc_indic, chill_indic, and scar_indic
+combinations <- expand.grid(forc_indic = c("yes", "no"),
+              chill_indic = c("yes", "no"),
+              scar_indic = c("yes", "no"))
+
+# Initialize an empty vector to store the number of unique species for each combination
+n_ssp <- vector()
+n_obs <- vector()
+
+# Loop through each combination
+for (i in 1:nrow(combinations)) {
+  # Get the current combination values
+  forc <- combinations$forc_indic[i]
+  chill <- combinations$chill_indic[i]
+  scar <- combinations$scar_indic[i]
+  
+  # Filter the data based on the current combination
+  filtered_data <- usda[usda$forc_indic == forc & usda$chill_indic == chill & usda$scar_indic == scar, ]
+  
+  # Count the number of unique species in the filtered data
+  unique_species <- length(unique(filtered_data$latbi))
+  
+  # Append the number to the vector
+  n_ssp <- c(n_ssp, unique_species)
+  
+  # Count the number of rows in the filtered data
+  unique_observations <- nrow(filtered_data)
+  
+  # Append the number of observations to the vector
+  n_obs <- c(n_obs, unique_observations)
+  
+  # Add the number to the combination table
+  combinations[i, "n_spp"] <- unique_species
+  combinations[i, "n_obs"] <- unique_observations
+}
+
+combinations
+
 
 
 
@@ -189,12 +244,12 @@ maxcols <- grep("Max", names(usda), value = TRUE)
 head(usda)
 names(usda)
 #how many species do we have with values in 
-tapply(usda$spec, usda$chill.dur.Max, function(x) length(unique(x)))
-tapply(usda$spec, usda$chill.dur.Max, function(x) (unique(x)))
+tapply(usda$latbi, usda$chill.dur.Max, function(x) length(unique(x)))
+tapply(usda$latbi, usda$chill.dur.Max, function(x) (unique(x)))
 
-aggregate(spec ~ chill.dur.Max, data = usda, FUN = function(x) length(unique(x)))
+aggregate(latbi ~ chill.dur.Max, data = usda, FUN = function(x) length(unique(x)))
 
-length(unique(usda[!is.na(usda$chill.dur.Max), "spec"])) #57 spp have a value in chill.dur.Max
+length(unique(usda[!is.na(usda$chill.dur.Max), "latbi"])) #57 spp have a value in chill.dur.Max
 
 
 
@@ -203,10 +258,10 @@ filtered_rows <- usda[!is.na(usda$chill.dur.Min) & !is.na(usda$chill.dur.Max) &
                       !is.na(usda$responseValueMin) & !is.na(usda$responseValueMax), ]
 
 filtered_spp <- usda[!is.na(usda$chill.dur.Min) & !is.na(usda$chill.dur.Max) & 
-                      !is.na(usda$responseValueMin) & !is.na(usda$responseValueMax), "spec"]
+                      !is.na(usda$responseValueMin) & !is.na(usda$responseValueMax), "latbi"]
 
 tmp_X <- filtered_rows$X
-spec_X <- filtered_rows$spec
+spec_X <- filtered_rows$latbi
 
 # Remove all rows filtered rows from the original data frame
 usda_new <- usda[!usda$X %in% tmp_X, ]
@@ -247,10 +302,10 @@ length(usda$X)
 #some more checks
 
 #extract species that have min/max values for chill duration and a corresponding min/max value in the response variable
-chilldurminnonNA <- usda$spec[which(!is.na(usda$chill.dur.Min))]
-chilldurmaxonNA <- usda$spec[which(!is.na(usda$chill.dur.Max))]
-respvarminnoNA <- usda$spec[which(!is.na(usda$responseValueMin))]
-respvarmaxnoNA <- usda$spec[which(!is.na(usda$responseValueMax))]
+chilldurminnonNA <- usda$latbi[which(!is.na(usda$chill.dur.Min))]
+chilldurmaxonNA <- usda$latbi[which(!is.na(usda$chill.dur.Max))]
+respvarminnoNA <- usda$latbi[which(!is.na(usda$responseValueMin))]
+respvarmaxnoNA <- usda$latbi[which(!is.na(usda$responseValueMax))]
 
 sppwithminmaxchill <- chilldurminnonNA[which(chilldurminnonNA %in% chilldurmaxonNA)]
 
@@ -261,30 +316,30 @@ sppwithcillresp <- sppwithminmaxchill[which(sppwithminmaxchill %in% sppwithminma
 spp_split<-unique(sppwithcillresp) #20 species have this combination and we can split them up
 
 ########## this option is no longer needed and limited but keep it for now
-#dublicate dataframe "usda" with rows containing a level in "spp_split" within usda$spec
-newmin <- usda[usda$spec %in% spp_split,]
+#dublicate dataframe "usda" with rows containing a level in "spp_split" within usda$latbi
+newmin <- usda[usda$latbi %in% spp_split,]
 #assign "NA" to all rows of column "chill.dur.Max" and "responseValueMax"
 newmin$chill.dur.Max <- NA
 newmin$responseValueMax <- NA
 
-#dublicate dataframe "usda" with rows containing a level in "spp_split" within usda$spec
-newmax <- usda[usda$spec %in% spp_split,]
+#dublicate dataframe "usda" with rows containing a level in "spp_split" within usda$latbi
+newmax <- usda[usda$latbi %in% spp_split,]
 #assign "NA" to all rows of column "chill.dur.Max" and "responseValueMax"
 newmax$chill.dur.Min <- NA
 newmax$responseValueMin <- NA
 
 str(newmax)
-newmax[,"spec"]
+newmax[,"latbi"]
 ## make a new dataframe: 
-# remove all rows from the "usda" containing a level in "spp_split" within usda$spec
-usda_new<-usda[!usda$spec %in% spp_split,]
+# remove all rows from the "usda" containing a level in "spp_split" within usda$latbi
+usda_new<-usda[!usda$latbi %in% spp_split,]
 
 #add the new dataframes "newmin" and "newmax" to the "usda" dataframe
 usda_new <- rbind(usda_new, newmin)
 usda_new <- rbind(usda_new, newmax)
 
 #add back rows of these species that didn't contain anything in "chill.dur.Min" and "chill.dur.Max".
-unique(usda_new[usda_new$spec,"spec"])
+unique(usda_new[usda_new$latbi,"latbi"])
 #add rows of these species that didn't contain anything in "responseValueMin" and "responseValueMax".
 
 #check
