3a674ec39b7b521d1d8a858f576a17476427581c
Selena Shew <shewselena@gmail.com>
2024-08-15 15:05:28 -0700

    finalized cleaning script for USDA phen & seed data, created cleaned outputs

--- analyses/scrapeUSDAseedmanual/cleaning/phen_seed_data_final_cleaning_script.R
+++ analyses/scrapeUSDAseedmanual/cleaning/phen_seed_data_final_cleaning_script.R
@@ -28,36 +28,71 @@ library(tidyr)
 library(readr)
 
 # Set Working directory (change as needed)
-# setwd("C:\\Users\\User\\my_repo_dir\\egret\\analyses\\scrapeUSDAseedmanual\\output")
+setwd("C:\\Users\\User\\my_repo_dir\\egret\\analyses\\scrapeUSDAseedmanual\\output")
 
-setwd("C:\\Users\\User\\my_repo_dir\\egret\\analyses\\scrapeUSDAseedmanual\\input\\test_folder")
+#setwd("C:\\Users\\User\\my_repo_dir\\egret\\analyses\\scrapeUSDAseedmanual\\input\\test_folder")
 
 # Read in data
-# phen_data <- read_csv('phenCom_added_missing_pagenum_genus.csv')
-# head(phen_data)
+
+# phen_data_test <- read.csv('phenCom_added_missing_pagenum_genus_test.csv', colClasses = "character")
+# head(phen_data_test)
 # 
-# seed_data <- read_csv('seedCom_added_missing_pagenum_genus.csv')
-# head(seed_data)
+# seed_data_test <- read.csv('seedCom_added_missing_pagenum_genus_test.csv', colClasses = "character")
+# head(seed_data_test)
 
-phen_data_test <- read.csv('phenCom_added_missing_pagenum_genus_test.csv', colClasses = "character")
-head(phen_data_test)
+phen_data <- read.csv('phenCom_added_missing_pagenum_genus.csv', colClasses = "character")
+head(phen_data)
 
-seed_data_test <- read.csv('seedCom_added_missing_pagenum_genus_test.csv', colClasses = "character")
-head(seed_data_test)
+seed_data <- read.csv('seedCom_added_missing_pagenum_genus.csv', colClasses = "character")
+head(seed_data)
 
 
-
+## Code
 # 1) Clean the phen_data
 
-phen_data_test <- phen_data_test %>%
+# test
+# phen_data_test <- phen_data_test %>%
+#   mutate(across(everything(), ~ na_if(.x, ""))) %>%
+#   mutate(across(everything(), ~ str_replace_all(.x, "NOT_SELECTED|NOT_SELECTED - |\\*|\\Â§", ""))) %>%
+#   fill(Species, .direction = "down") %>%
+#   mutate(Species = str_remove_all(Species, "\\b[A-Z]\\. ")) %>%
+#   mutate(across(everything(), ~ ifelse(.x == ".", NA, .x))) %>%
+#   mutate(across(everything(), ~ str_replace_all(.x, "\\|", "1")))
+
+phen_data <- phen_data %>%
   mutate(across(everything(), ~ na_if(.x, ""))) %>%
   mutate(across(everything(), ~ str_replace_all(.x, "NOT_SELECTED|NOT_SELECTED - |\\*|\\Â§", ""))) %>%
   fill(Species, .direction = "down") %>%
   mutate(Species = str_remove_all(Species, "\\b[A-Z]\\. ")) %>%
   mutate(across(everything(), ~ ifelse(.x == ".", NA, .x))) %>%
   mutate(across(everything(), ~ str_replace_all(.x, "\\|", "1")))
+
+
+# 2) Clean the seed_data
+ 
+# test
+# seed_data_test <- seed_data_test %>%
+#   mutate(across(everything(), ~ na_if(.x, ""))) %>%
+#   mutate(across(everything(), ~ str_replace_all(.x, "NOT_SELECTED|NOT_SELECTED - |\\*|\\Â§", ""))) %>%
+#   fill(Species, .direction = "down") %>%
+#   mutate(Species = str_remove_all(Species, "\\b[A-Z]\\. ")) %>%
+#   mutate(across(everything(), ~ ifelse(.x == ".", NA, .x))) %>%
+#   mutate(across(everything(), ~ str_replace_all(.x, "\\|", "1")))
+
+seed_data <- seed_data %>%
+  mutate(across(everything(), ~ na_if(.x, ""))) %>%
+  mutate(across(everything(), ~ str_replace_all(.x, "NOT_SELECTED|NOT_SELECTED - |\\*|\\Â§", ""))) %>%
+  fill(Species, .direction = "down") %>%
+  mutate(Species = str_remove_all(Species, "\\b[A-Z]\\. ")) %>%
+  mutate(across(everything(), ~ ifelse(.x == ".", NA, .x))) %>%
+  mutate(across(everything(), ~ str_replace_all(.x, "\\|", "1")))
+
   
-# 2) Output the cleaned phen_data to a new CSV
+# 3) Output the cleaned data to a new CSV
 
-write_csv(phen_data_test, "cleaned_phen_data_test.csv")
+# test
+# write_csv(phen_data_test, "cleaned_phen_data_test.csv")
+# write_csv(seed_data_test, "cleaned_seed_data_test.csv")
 
+write_csv(phen_data, "cleaned_phen_data_final.csv")
+write_csv(seed_data, "cleaned_seed_data_final.csv")
