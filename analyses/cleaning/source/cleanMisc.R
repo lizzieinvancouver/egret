cfaa467c8eb7b6e09d71724ef2ff9c7b360de5b6
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2024-12-10 12:21:57 -0800

    double checking clean Misc and datasetID

--- analyses/cleaning/source/cleanMisc.R
+++ analyses/cleaning/source/cleanMisc.R
@@ -10,30 +10,38 @@
 d <- d[-(which(d$datasetID == "lee21" & d$figure %in% c("Figure 1c", "Figure 1b", "Figure 2a", "Figure 3b"))),]
 
 #na11: the description of the treatments vary between the methods and the results, so we are removing this study
-
 d <- d[-(which(d$datasetID == "na11")),]
 
+# datasets that were entered my multiple people to check for data consistency, but keeping only one
 d <- d[-(which(d$datasetID == "batlla03" & d$entered.by == "DM")),]
 d <- d[-(which(d$datasetID == "chen15" & d$entered.by == "AZ")),]
+d <- d[-which(d$datasetID == "al-absi10" & d$entered.by == "TA"),]  #96 rows to remove
+d <- d[-which(d$datasetID == "chen06" & d$entered.by == "AZ"),] #96
+d <- d[-which(d$datasetID == "chichizola18" & d$entered.by == "AZ"),] #45
+d <- d[-which(d$datasetID == "han10" & d$entered.by == "CRD"),] #40
+d <- d[-which(d$datasetID == "lee21" & d$entered.by == "AZ"),] #30
+d <- d[-which(d$datasetID == "moeini21" & d$entered.by == "MN"),] #45
+d <- d[-which(d$datasetID == "tilki07" & d$entered.by == "MN"),] #104
+d <- d[-which(d$datasetID == "wytsalucy21" & d$entered.by == "DK"),]#92
+d <- d[-which(d$datasetID == "yusefi-tanha19" & d$entered.by == "JS"),]#40
 
-#d <- d[-(which(d$datasetID == "chen15" & d$entered.by == "TA")),]
-
-d <- d[-(which(d$datasetID == "jang22" & d$entered.by == "SC")),]
-d <- d[-(which(d$datasetID == "langlois17" & d$entered.by == "AZ")),]
-d <- d[-(which(d$datasetID == "lo19" & d$entered.by == "SC" & d$figure == "Figure 2")),]
-d <- d[-(which(d$datasetID == "nurse08" & d$entered.by == "MN")),]
-
-
-d <- d[-(which(d$datasetID == "olmez07" & d$entered.by == "MN" & d$figure == "Table 2")),]
-d <- d[-(which(d$datasetID == "olmez09" & d$entered.by == "MN" & d$figure == "Table 3")),]
-d <- d[-(which(d$datasetID == "redondo-gomez11" & d$entered.by == "DM")),]
-d <- d[-(which(d$datasetID == "zhou08" & d$entered.by == "DK")),]
-
-
-# 12 papers were rescraped by SS and corrected by DL:
+# 10 papers were re-scraped by SS and corrected by DL:
 corrected <- c("batlla03","chen15","jang22","langlois17", "lo19", "nurse08","olmez07","olmez09", "redondo-gomez11", "zhou08")     
 
-temp <- d[d$datasetID %in% corrected,]
+d <- d[-which(d$datasetID == "chen15" & d$entered.by == "TA"),] #96
+d <- d[-which(d$datasetID == "langlois17" & d$entered.by == "AZ"),] #40
+d <- d[-which(d$datasetID == "lo19" & d$entered.by == "SC"),] #30
+d <- d[-which(d$datasetID == "nurse08" & d$entered.by == "MN"),] #45
+d <- d[-which(d$datasetID == "olmez07" & d$entered.by == "MN"),] #104
+d <- d[-which(d$datasetID == "olmez09" & d$entered.by == "MN"),]#92
+d <- d[-which(d$datasetID == "redondo-gomez11" & d$entered.by == "DM"),]#40
+d <- d[-which(d$datasetID == "zhou08" & d$entered.by == "DK"),]  #96 rows to remove
+d <- d[-which(d$datasetID == "pritchard93" & d$entered.by == "DM"),]  #96 rows to remove
+
+
+# d <- d[-(which(d$datasetID == "olmez07" & d$entered.by == "MN" & d$figure == "Table 2")),]
+# d <- d[-(which(d$datasetID == "olmez09" & d$entered.by == "MN" & d$figure == "Table 3")),]
+
 if(FALSE){
 ##	
 ## From clean_other.R, Original file called coordinate_cleaning_JS.R
--- analyses/cleaning/source/cleandatasetID.R
+++ analyses/cleaning/source/cleandatasetID.R
@@ -5,64 +5,55 @@
 
 # Fix ones with incorrect years
 d$datasetID[which(d$entered.by == "DMB")] <- "veiga-barbosa14"
-d$datasetID[which(d$datasetID == "acosta12")] <- "acosta13"
-d$datasetID[which(d$datasetID == "brandel2005")] <- "brandel05"
+d$datasetID[which(d$datasetID == "Acosta12")] <- "acosta13"
+d$datasetID[which(d$datasetID == "Brandel2005")] <- "brandel05"
 d$datasetID[which(d$datasetID == "airi2009")] <- "airi09"
-d$datasetID[which(d$datasetID == "alptekin2002")] <- "alptekin02"
-d$datasetID[which(d$datasetID == "amini2018")] <- "amini18" 
+d$datasetID[which(d$datasetID == "Alptekin2002")] <- "alptekin02"
+d$datasetID[which(d$datasetID == "Amini2018")] <- "amini18" 
 d$datasetID[which(d$datasetID == "pipinus12")] <- "pipinis12"
 d$datasetID[which(d$datasetID == "picciau18")] <- "picciau19"
 d$datasetID[which(d$datasetID == "li11 ")] <- "li11"
-
-d$datasetID[which(d$datasetID == "yang18" & d$species == "taiwanensis")] <- "yang18_1"
-d$datasetID[which(d$datasetID == "yang18" & d$species == "glabra")] <- "yang18_2"
-d$datasetID[which(d$datasetID == "yang18" & d$species == "taccada")] <- "yang18_3"
-
-d$datasetID[which(d$datasetID == "yang16" & d$species == "taccada")] <- "yang18_3"
-
-# Checked, these are correct: Kolodziejek18,19, Meyer94,95; Olmez07-09; Rouhi12,13; Tilki06,07; tylkowski09-10
-d$datasetID[which(d$datasetID == "Aldridge1993")] <- "aldridge1992"
-d$datasetID[which(d$datasetID == "Aldridge1994")] <- "aldridge1992"
-d$datasetID[which(d$datasetID == "Aldridge1995")] <- "aldridge1992"
-d$datasetID[which(d$datasetID == "Aldridge1996")] <- "aldridge1992"
-d$datasetID[which(d$datasetID == "Aldridge1997")] <- "aldridge1992"
-
-d$datasetID[which(d$datasetID == "irvani13")] <- "irvani12"
-d$datasetID[which(d$datasetID == "irvani14")] <- "irvani12"
-
 d$datasetID[which(d$datasetID == "Sacande05")] <- "sacande04"
+
+d$datasetID[which(d$datasetID == "tang10a")] <- "tang10_1"
+d$datasetID[which(d$datasetID == "tang10b")] <- "tang10_2"
 
 # three yang18
 d$datasetID[which(d$datasetID == "yang18" & d$genus == "Maackia")] <- "yang18_1"
 d$datasetID[which(d$datasetID == "yang18" & d$genus == "Pasania")] <- "yang18_2"
 d$datasetID[which(d$datasetID == "yang18" & d$genus == "Scaevola")] <- "yang18_3"
 
-d$datasetID[which(d$datasetID == "tang10a")] <- "tang10_1"
-d$datasetID[which(d$datasetID == "tang10b")] <- "tang10_2"
+# two yang16, one already yang16_1
+d$datasetID[which(d$datasetID == "yang16")] <- "yang16_2"
+
+# Checked, these are correct: Kolodziejek18,19, Meyer94,95; Olmez07-09; Rouhi12,13; Tilki06,07; tylkowski09-10
+d$datasetID[which(d$datasetID == "Aldridge1992")] <- "aldridge92"
+d$datasetID[which(d$datasetID == "Aldridge1993")] <- "aldridge92"
+d$datasetID[which(d$datasetID == "Aldridge1994")] <- "aldridge92"
+d$datasetID[which(d$datasetID == "Aldridge1995")] <- "aldridge92"
+d$datasetID[which(d$datasetID == "Aldridge1996")] <- "aldridge92"
+d$datasetID[which(d$datasetID == "Aldridge1997")] <- "aldridge92"
+d$datasetID[which(d$datasetID == "Aldridge1998")] <- "aldridge92"
+
+d$datasetID[which(d$datasetID == "irvani13")] <- "irvani12"
+d$datasetID[which(d$datasetID == "irvani14")] <- "irvani12"
+
+d$datasetID[which(d$datasetID == "Redondo-gomez12")] <- "redondo-gomez11"
+d$datasetID[which(d$datasetID == "Redondo-gomez13")] <- "redondo-gomez11"
+
 d$datasetID <- tolower(d$datasetID)
 
-temp <- unique(d[,c("datasetID", "entered.by")])
-temp$count <-1
-temp$datasetID <- tolower(temp$datasetID)
-tempy <- aggregate(temp["count"], temp[c("datasetID")], FUN = sum)
-
-d <- d[-which(d$datasetID == "al-absi10" & d$entered.by == "TA"),]  #96 rows to remove
-d <- d[-which(d$datasetID == "chen06" & d$entered.by == "AZ"),] #96
-d <- d[-which(d$datasetID == "chen15" & d$entered.by == "TA"),] #11
-d <- d[-which(d$datasetID == "chichizola18" & d$entered.by == "AZ"),] #45
-
-d <- d[-which(d$datasetID == "han10" & d$entered.by == "CRD"),] #40
-d <- d[-which(d$datasetID == "lee21" & d$entered.by == "AZ"),] #30
-d <- d[-which(d$datasetID == "moeini21" & d$entered.by == "MN"),] #45
-d <- d[-which(d$datasetID == "tilki07" & d$entered.by == "MN"),] #104
-d <- d[-which(d$datasetID == "wytsalucy21" & d$entered.by == "DK"),]#92
-d <- d[-which(d$datasetID == "yusefi-tanha19" & d$entered.by == "JS"),]#40
-
+# cleaned entered.by 
 d$entered.by[which(d$entered.by == "DL ")] <- "DL"
+
+# temp <- unique(d[,c("datasetID", "entered.by")])
+# temp$count <-1
+# temp$datasetID <- tolower(temp$datasetID)
+# tempy <- aggregate(temp["count"], temp[c("datasetID")], FUN = sum)
 
 # ident <- read.csv("..//data/datasetID.csv")
 # ident$dup <- duplicated(ident$studyID)
 
 #I have double checked all the papers that have duplicates
-# many have not been entered for various reasons, others the duplicated is in another language and therefore have not be entered
+# many have not been entered for various legitamite reasons, others the duplicated is in another language and therefore have not be entered
 
