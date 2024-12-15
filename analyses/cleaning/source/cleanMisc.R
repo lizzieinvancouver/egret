831287f802f6a00fe969d482b858100629fc2e4b
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2024-12-14 16:59:19 -0800

    triple checked cleanMisc

--- analyses/cleaning/source/cleanMisc.R
+++ analyses/cleaning/source/cleanMisc.R
@@ -7,15 +7,15 @@
 ## or ... it should, once we have stuff like that ... ##
 
 # Lee 21 table2 and Fig 1c redundant data, subset to table only
-d <- d[-(which(d$datasetID == "lee21" & d$figure %in% c("Figure 1c", "Figure 1b", "Figure 2a", "Figure 3b"))),]
+d <- d[-(which(d$datasetID == "lee21" & d$figure %in% c("Figure 1c", "Figure 1b", "Figure 2a", "Figure 3b"))),] # 112 rows
 
 #na11: the description of the treatments vary between the methods and the results, so we are removing this study
-d <- d[-(which(d$datasetID == "na11")),]
+d <- d[-(which(d$datasetID == "na11")),] #12 rows
 
 # datasets that were entered my multiple people to check for data consistency, but keeping only one
-d <- d[-(which(d$datasetID == "batlla03" & d$entered.by == "DM")),]
-d <- d[-(which(d$datasetID == "chen15" & d$entered.by == "AZ")),]
-d <- d[-which(d$datasetID == "al-absi10" & d$entered.by == "TA"),]  #96 rows to remove
+d <- d[-(which(d$datasetID == "batlla03" & d$entered.by == "DM")),] #34
+d <- d[-(which(d$datasetID == "chen15" & d$entered.by == "AZ")),] #257
+d <- d[-which(d$datasetID == "al-absi10" & d$entered.by == "TA"),]  #96 
 d <- d[-which(d$datasetID == "chen06" & d$entered.by == "AZ"),] #96
 d <- d[-which(d$datasetID == "chichizola18" & d$entered.by == "AZ"),] #45
 d <- d[-which(d$datasetID == "han10" & d$entered.by == "CRD"),] #40
@@ -28,19 +28,20 @@ d <- d[-which(d$datasetID == "yusefi-tanha19" & d$entered.by == "JS"),]#40
 # 10 papers were re-scraped by SS and corrected by DL:
 corrected <- c("batlla03","chen15","jang22","langlois17", "lo19", "nurse08","olmez07","olmez09", "redondo-gomez11", "zhou08")     
 
-d <- d[-which(d$datasetID == "chen15" & d$entered.by == "TA"),] #96
-d <- d[-which(d$datasetID == "langlois17" & d$entered.by == "AZ"),] #40
-d <- d[-which(d$datasetID == "lo19" & d$entered.by == "SC"),] #30
-d <- d[-which(d$datasetID == "nurse08" & d$entered.by == "MN"),] #45
-d <- d[-which(d$datasetID == "olmez07" & d$entered.by == "MN"),] #104
-d <- d[-which(d$datasetID == "olmez09" & d$entered.by == "MN"),]#92
-d <- d[-which(d$datasetID == "redondo-gomez11" & d$entered.by == "DM"),]#40
-d <- d[-which(d$datasetID == "zhou08" & d$entered.by == "DK"),]  #96 rows to remove
-d <- d[-which(d$datasetID == "pritchard93" & d$entered.by == "DM"),]  #96 rows to remove
+d <- d[-which(d$datasetID == "chen15" & d$entered.by == "TA"),] #11
+d <- d[-which(d$datasetID == "airi09" & d$entered.by == "TA"),] # 90
+d <- d[-which(d$datasetID == "langlois17" & d$entered.by == "AZ"),] #322
+d <- d[-which(d$datasetID == "lo19" & d$entered.by == "SC"),] #95
+d <- d[-which(d$datasetID == "nurse08" & d$entered.by == "MN"),] #156
+d <- d[-which(d$datasetID == "olmez07" & d$entered.by == "MN"),] #92
+d <- d[-which(d$datasetID == "olmez09" & d$entered.by == "MN"),] #56
+d <- d[-which(d$datasetID == "redondo-gomez11" & d$entered.by == "DM"),]#142
+d <- d[-which(d$datasetID == "zhou08" & d$entered.by == "DK"),]  #43
+d <- d[-which(d$datasetID == "pritchard93" & d$entered.by == "MN" & d$figure == "Table 3"),]  #6 
 
 # momonoki79: issue #65 these two tables switch the seeds to lettuce---a crop
-d <- d[-(which(d$datasetID == "momonoki79" & d$figure == "Table 20")),]
-d <- d[-(which(d$datasetID == "momonoki79" & d$figure == "Table 21")),]
+d <- d[-(which(d$datasetID == "momonoki79" & d$figure == "table20")),] # 4
+d <- d[-(which(d$datasetID == "momonoki79" & d$figure == "table21")),] # 4
 
 if(FALSE){
 ##	
