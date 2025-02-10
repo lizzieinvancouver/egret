ebe0d5a2e0b4a076dac3a750a37dd38ed51000a5
wangxm-forest <wangxiaomao0209@gmail.com>
2025-02-10 13:21:15 -0800

    Updated USDA cleaning code: cleaned scarification

--- analyses/scrapeUSDAseedmanual/cleaning/source/cleanPreTrtUsda.R
+++ analyses/scrapeUSDAseedmanual/cleaning/source/cleanPreTrtUsda.R
@@ -20,14 +20,10 @@ d$pretreatment <- gsub("-"," to ",d$pretreatment)
 d$pretreatmentFeces <- d$pretreatment
 d$pretreatmentChill <- d$pretreatment
 d$pretreatmentChillDuration <- d$pretreatment
-d$pretreatmentScarifType <- d$pretreatment
-d$pretreatmentScarifTypeGen <- d$pretreatment
 
 d$pretreatmentFeces[!grepl("feces", d$pretreatmentFeces)] <- NA
 d$pretreatmentChill[!grepl("chill|stratification", d$pretreatmentChill)] <- NA
 d$pretreatmentChillDuration[!grepl("chill|stratification", d$pretreatmentChillDuration)] <- NA
-d$pretreatmentScarifType[!grepl("scarification|Scarification", d$pretreatmentScarifType)] <- NA
-d$pretreatmentScarifTypeGen[!grepl("scarification|Scarification", d$pretreatmentScarifTypeGen)] <- NA
 d$pretreatment[!is.na(d$pregermination_treatment_hot_water_soak_C)] <- "Hot water"
 
 d$pretreatmentChill[which(grepl("chill|stratification",d$pretreatmentChill))] <- "Y"
@@ -46,24 +42,3 @@ d$pretreatmentChillDuration[which(d$pretreatmentChillDuration == "3 to 5 month s
 d$pretreatmentChillDuration[which(d$pretreatmentChillDuration == "6 to 9 month stratification")] <- "182.5 to 273.75"
 d$pretreatmentChillDuration[which(d$pretreatmentChillDuration == "10 to 13 month stratification")] <- "304.167 to 395.417"
 
-d$pretreatmentScarifType[which(d$pretreatment == "H2SO4")] <- "H2SO4"
-d$pretreatmentScarifType[which(d$pretreatment == "Mech")] <- "Mechanical"
-d$pretreatmentScarifType[which(d$pretreatment == "Mech.")] <- "Mechanical"
-d$pretreatmentScarifType[which(d$pretreatment == "Heat")] <- "Heat"
-d$pretreatmentScarifType[which(d$pretreatment == "Hot water")] <- "Hot water"
-d$pretreatmentScarifType[which(d$pretreatment == "Warm water")] <- "Warm water"
-d$pretreatmentScarifType[which(d$pretreatment == "Abrasion")] <- "Abrasion"
-d$pretreatmentScarifType[which(d$pretreatment == "Acid soak")] <- "Acid soak"
-d$pretreatmentScarifType[which(d$pretreatment == "Nicking")] <- "Nicking"
-d$pretreatmentScarifType[which(d$pretreatment == "No scarification")] <- NA
-
-d$pretreatmentScarifTypeGen[which(grepl("water|Water", d$pretreatmentScarifType))] <- "Moisture"
-d$pretreatmentScarifTypeGen[which(grepl("H2SO4|Acid|acid", d$pretreatmentScarifType))] <- "Chemical"
-d$pretreatmentScarifTypeGen[which(grepl("Mechanical|Abrasion|Nicking", d$pretreatmentScarifType))] <- "Mechanical"
-d$pretreatmentScarifTypeGen[which(grepl("Heat", d$pretreatmentScarifType))] <- "Thermal"
-
-# Rubus spectabilis has general scarification, will find
-d$pretreatmentScarifType[which(d$pretreatmentScarifType == "Scarification & 2 month stratification")] <- "Acid scarification"
-d$pretreatmentScarifType[which(d$pretreatmentScarifType == "Scarification & 4 month stratification")] <- "Acid scarification"
-d$pretreatmentScarifType[which(d$pretreatmentScarifType == "Scarification & 6 month stratification")] <- "Acid scarification"
-d$pretreatmentScarifTypeGen[which(d$pretreatmentScarifType == "Acid scarification")] <- "Chemical"
---
+++ analyses/scrapeUSDAseedmanual/cleaning/source/cleanScarificationUsda.R
@@ -0,0 +1,43 @@
+## Started 10 Feb by Mao ##
+
+d$pretreatmentScarifTypeGen <- d$pretreatment
+d$pretreatmentScarifTypeSpe <- d$pretreatment
+d$pretreatmentScarifTypeGen[!grepl("scarification|Scarification|H2SO4|Mech|Heat|water|Abrasion|Acid|acid", d$pretreatmentScarifTypeGen)] <- NA
+d$pretreatmentScarifTypeSpe[!grepl("scarification|Scarification|H2SO4|Mech|Heat|water|Abrasion|Acid|acid", d$pretreatmentScarifTypeSpe)] <- NA
+
+# Clean general scarification column
+d$pretreatmentScarifTypeGen[which(grepl("H2SO4|Acid|acid", d$pretreatmentScarifTypeGen))] <- "chemical"
+d$pretreatmentScarifTypeGen[which(grepl("water", d$pretreatmentScarifTypeGen))] <- "soaking"
+d$pretreatmentScarifTypeGen[which(grepl("Mech|Abrasion|Nicking", d$pretreatmentScarifTypeGen))] <- "mechanical"
+d$pretreatmentScarifTypeGen[which(d$pretreatment == "Heat")] <- "mechanical"
+d$pretreatmentScarifTypeGen[which(d$pretreatment == "No scarification")] <- NA
+d$pretreatmentScarifTypeGen[which(grepl("scarification|Scarification", d$pretreatmentScarifTypeGen))] <- "unknownScarification"
+# Rubus spectabilis has general scarification, will find
+d$pretreatmentScarifTypeGen[which(d$pretreatmentScarifTypeGen == "Scarification & 2-mon stratification")] <- "chemical"
+d$pretreatmentScarifTypeGen[which(d$pretreatmentScarifTypeGen == "Scarification & 4-mon stratification")] <- "chemical"
+d$pretreatmentScarifTypeGen[which(d$pretreatmentScarifTypeGen == "Scarification & 6-mon stratification")] <- "chemical"
+
+# Clean specific scarification column
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "H2SO4")] <- "checmical - H2SO4"
+d$pretreatmentScarifTypeSpe[which(grepl("Mech", d$pretreatmentScarifTypeSpe))] <- "mechanical - unknown"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Heat")] <- "mechanical - heat"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "77-100")] <- "soaking - 77°C-100°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "71-91")] <- "soaking - 71°C-91°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "90")] <- "soaking - 90°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "85")] <- "soaking - 85°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "80")] <- "soaking - 80°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "71")] <- "soaking - 71°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "120")] <- "soaking - 120°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "100")] <- "soaking - 100°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pregermination_treatment_hot_water_soak_C == "70")] <- "soaking - 70°C hot water"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Hot water")] <- "soaking - hot water"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Warm water")] <- "soaking - warm water"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Abrasion")] <- "mechanical - abrasion"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Acid soak")] <- "chemical - acid"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Stratification")] <- "unknowStratification"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Acid scarification")] <- "checmical - acid"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Scarification & 2-mon stratification")] <- "chemical - acid"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Scarification & 4-mon stratification")] <- "chemical - acid"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Scarification & 6-mon stratification")] <- "chemical - acid"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "Nicking")] <- "mechanical - nicking"
+d$pretreatmentScarifTypeSpe[which(d$pretreatmentScarifTypeSpe == "No scarification")] <- NA
