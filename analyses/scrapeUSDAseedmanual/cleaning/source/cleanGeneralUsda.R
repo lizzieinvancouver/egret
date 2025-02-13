f24db4593501fc48c3531005e9143fadf9dbb932
wangxm-forest <wangxiaomao0209@gmail.com>
2025-02-13 12:30:22 -0800

    Updated USDA cleaning code, remove empty rows much earlier and fixed responseVar NAs issue

--- analyses/scrapeUSDAseedmanual/cleaning/source/cleanGeneralUsda.R
+++ analyses/scrapeUSDAseedmanual/cleaning/source/cleanGeneralUsda.R
@@ -7,7 +7,6 @@
 # Giving each row a unique number so I can fix the issue where species and variety name are the same easily
 d <- d %>%
   rowid_to_column("rowID")
-
 # Removing apostrophe across all cells
 d[] <- lapply(d, gsub, pattern="'", replacement="")
 
@@ -42,11 +41,17 @@ d <- mutate_all(d, ~ gsub("-$", "", .))
 d <- d %>%
   distinct()
 
+# Replace all empty cells with NA
+d <- replace(d, d=='', NA)
+
 # Remove empty columns
 na_columns <- sapply(d, function(x) all(is.na(x)))
 
 d <- d[, !na_columns]
 
+# Remove rows with no data at all
+d <- d %>%
+  filter(!if_all(c(pregermination_treatment_time_minutes,pregermination_treatment_hot_water_soak_C,pretreatment,stratification_temp_C,warm_stratification_days,cold_stratification_days,dailyl_light_hours,day_temp_celsius,night_temp_celsius,temp_unspecified_time_of_day_celsius,test_duration_in_days, germination_time_days, total_germination_percent, avg_germination_percent, samples, germination_rate, avg_germinative_energy_percent, germinative_energy_percent, avg_germinative_capacity, germinative_capacity, percent_germination_15degC_incubated, percent_germination_20degC_incubated), is.na))
 # Clean some columns without specific cleaning code for other stuff
 d$samples <- gsub("-"," to ",d$samples)
 d$cold_stratification_days <- gsub("-"," to ",d$cold_stratification_days)
