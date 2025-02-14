f6d7e9d87768bb113daaac50559df2e2a4e64702
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2025-02-14 10:39:40 -0800

    code merge egret usda

---
+++ analyses/analyseSeedCues/mergeEgretUSDA.R
@@ -0,0 +1,50 @@
+# started Feb 13, 2025 by Deirdre
+# aim of this code is to merge the egret and the usda data together:
+# housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+if(length(grep("deirdre", getwd()) > 0)) {
+  setwd("~/Documents/github/egret/analyses")
+} else if(length(grep("lizzie", getwd()) > 0)) {
+  setwd("/Users/lizzie/Documents/git/projects/egret/analyses")
+} else if(length(grep("sapph", getwd()) > 0)) {
+  setwd("/Users/sapph/Documents/ubc things/work/egret/analyses")
+} else if(length(grep("danielbuonaiuto", getwd()) > 0)) {
+  setwd("/Users/danielbuonaiuto/Documents/git/egret/analyses")
+} else if(length(grep("Xiaomao", getwd()) > 0)) {
+  setwd("C:/PhD/Project/egret/analyses")
+} else if(length(grep("britanywuuu", getwd()) > 0)) {
+  setwd("/Documents/ubc/year5/TemporalEcologyLab/egret/analyses")
+} else if(length(grep("Ken", getwd())) > 0){
+  setwd("/Users/Ken Michiko Samson/Documents/Temporal Ecology Lab/egret/analyses")
+} else if(length(grep("christophe_rouleau-desrochers", getwd())) > 0){
+  setwd("/Users/christophe_rouleau-desrochers/github/egret/analyses")
+} else if(length(grep("victor", getwd())) > 0){
+  setwd('~/projects/egret/analyses')
+} 
+
+u <- read.csv("input/usdaGerminationCleaned.csv")
+d <- read.csv("output/egretclean.csv")
+
+
+rmCol <- c("speciesID", "filePath", "pdfPageNumber", "scrapedTableNumber","pdfTableNumber", "medium", "")
+
+# seedType: includes endocarp removed ---seems like scarification
+# pretreatment: 
+# usda <- u[, c("genus", "species", "seedType", "source.population", "medium", "pretreatmentDuration",
+#               "stratificationTemp", "warmStratDuration", "coldStratDuration","chillDurationAvg", "germDurationAvg",  "photoperiodAvg", "responseValueAvg" 
+#               
+#               "tempDay"                  "tempNight"               
+# [21] "tempUnspecified"          "germDuration"             "samples"                  "latbi"                   
+# [25] "chilling"                 "chillDuration"            "scarifTypeGen"            "scarifTypeSpe"           
+# [29] "responseVar"              "responseValue"            "pretreatmentMin"          "pretreatmentMax"         
+# [33] "coldStratDurMin"          "coldStratDurMax"                  
+# [37] "tempDayMin"               "tempDayMax"               "tempNightMin"             "tempNightMax"            
+# [41] "germDurationMin"          "germDurationMax"          "samplesMin"               "samplesMax"              
+# [45] "chillDurationMin"         "chillDurationMax"             "pretreatmentAvg"          "coldStratDurAvg"            
+# [53] "tempDayAvg"               "tempNightAvg"             "germDurationAvg"          "samplesAvg"              
+# [57] "chillDurationAvg"        )]
+
+
+
