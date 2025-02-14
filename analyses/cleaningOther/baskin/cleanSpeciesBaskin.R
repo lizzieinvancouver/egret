88d1068f49d448a1c175dab93ce6bde7d659b896
wangxm-forest <wangxiaomao0209@gmail.com>
2025-02-14 11:25:35 -0800

    Baskin USDA species name overlapping cleaning

--- analyses/cleaningOther/baskin/cleanSpeciesBaskin.R
+++ analyses/cleaningOther/baskin/cleanSpeciesBaskin.R
@@ -8,8 +8,8 @@ baskin <- read.csv("input/Baskin_Dormancy_Database.csv", skip=2)
 baskin$X1 <- NULL
 # Substitute the underscore with space
 baskin$Genus_species <- sub("_", " ", baskin$Genus_species)
-# Use worldFlora package to inspect whether names are correct
-runworldflora <- TRUE
+# Use worldFlora package to inspect whether names are correct, this takes ages to run, so I saved the checks
+runworldflora <- FALSE
 if(runworldflora){	
   library("stringr")
   library("WorldFlora")
@@ -20,7 +20,10 @@ if(runworldflora){
   names_changed <- setdiff(baskin$Genus_species, d_species_fix)
   names_changed
 }
-write.csv(checks, "cleaningOther/baskin/checksFull.csv")
+
+# write.csv(checks, "cleaningOther/baskin/checksFull.csv")
+
+checks <- read.csv("cleaningOther/baskin/checksFull.csv", header = T)
 # Ok, there are way too many species names need to be fixed, including species we might not end up using.
 # Only correct species names appears in egret dataset
 source("cleaning/source/mergeData.R") 
@@ -89,5 +92,93 @@ baskin_egret$Genus_species[which(baskin_egret$Genus_species == "Zannichellia pal
 baskin_egret$Genus_species[which(baskin_egret$Genus_species == "Zannichellia pedunculata")] <- "Ruppia maritima"
 baskin_egret <- unique(baskin_egret)
 
-write.csv(baskin_egret,"output/baskinegretclean.csv")
+write.csv(baskin_egret,"output/baskinegretclean.csv", row.names = FALSE)
+
+#Only correct species names appears in USDA dataset
+
+d <- read_csv("scrapeUSDAseedmanual/cleaning/germPreCleanedMasterSheet.csv", na = c("", "NA"))
+source("scrapeUSDAseedmanual/cleaning/source/cleanGeneralUsda.R")
+source("scrapeUSDAseedmanual/cleaning/source/cleanSpeciesUsda.R")
+
+usda_sp <- paste(d$genus_name, d$species_name)
+d_species_fix <- unique(checks$scientificName)
+shared_sp <- intersect(usda_sp, d_species_fix)
+shared_sp_list <- checks[checks$scientificName %in% shared_sp, ]
+shared_sp_list <- shared_sp_list[, c("spec.name","spec.name.ORIG", "scientificName")]
+baskin_sp_overlap <- intersect(shared_sp_list$spec.name.ORIG, baskin$Genus_species)
+baskin_sp_overlap
+
+baskin_usda <- baskin[baskin$Genus_species %in% baskin_sp_overlap, ]
+shared_sp_list$Genus_species <- shared_sp_list$spec.name.ORIG
+shared_sp_list <- unique(shared_sp_list)
+sp_name_changed <- merge(baskin_usda,shared_sp_list, by = "Genus_species", all.x = TRUE)
+names_changed <- setdiff(sp_name_changed$Genus_species, sp_name_changed$scientificName)
+names_changed
+
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Acacia farnesiana")] <- "Vachellia farnesiana"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Acacia nilotica")] <- "Vachellia nilotica"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Acer ginnala")] <- "Acer tataricum subsp. ginnala"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Acer saccharum -grandidentatum")] <- "Acer saccharum"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Aesculus octandra")] <- "Aesculus flava"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Alnus crispa")] <- "Alnus alnobetula subsp. crispa"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Alnus sinuata")] <- "Alnus alnobetula subsp. sinuata"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Alnus tenuifolia")] <- "Alnus incana subsp. tenuifolia"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Carya illinoinsis")] <- "Carya illinoinensis"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Ceanothus americana")] <- "Ceanothus americanus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Ceanothus divaricatus")] <- "Ceanothus oliganthus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Ceanothus greggii")] <- "Ceanothus pauciflorus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Ceanothus ledifolius")] <- "Ceanothus americanus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Ceanothus sorediatus")] <- "Ceanothus arboreus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Cornus florida var. urbiniana")] <- "Cornus florida"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Cornus stolonifera")] <- "Cornus sericea"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Cotoneaster acutifolia")] <- "Cotoneaster acutifolius"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Cotonester acutifolius")] <- "Cotoneaster acutifolius"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Cotonester microphyllus")] <- "Cotoneaster horizontalis"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Crataegus anomala")] <- "Crataegus holmesiana"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Crataegus cordata")] <- "Crataegus phaenopyrum"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Crataegus flava")] <- "Crataegus punctata"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Crataegus rivularis")] <- "Crataegus douglasii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Euonymus atropurpureusW")] <- "Euonymus atropurpureus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Euonymus verrucosa")] <- "Euonymus verrucosus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Fraxinus lanceolata")] <- "Fraxinus pennsylvanica"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Mahonia aquifolium")] <- "Berberis aquifolium"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Mahonia fremontii")] <- "Berberis fremontii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Mahonia nevinii")] <- "Berberis nevinii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Mahonia repens")] <- "Berberis repens"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Padus racemosa")] <- "Prunus padus"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Philadelphus lewisi")] <- "Philadelphus lewisii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Pinus excelsa")] <- "Pinus wallichiana"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Pinus khasya")] <- "Pinus kesiya"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Pinus leucodermis")] <- "Pinus heldreichii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Pinus longifolia")] <- "Pinus roxburghii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Pinus massoniana")] <- "Pinus thunbergii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Prosopis juliflora")] <- "Neltuma juliflora"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Pyrus arbutifolia")] <- "Aronia arbutifolia"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus borealis")] <- "Quercus petraea"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus emoryi")] <- "Quercus sinuata"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus lanuginosa")] <- "Quercus robur"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus nuttallii")] <- "Quercus texana"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus palustris")] <- "Quercus coccinea"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus prinus")] <- "Quercus michauxii"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus pyrenaica")] <- "Quercus robur"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus vaccinifolia")] <- "Quercus vacciniifolia"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Quercus wislizenii")] <- "Quercus wislizeni"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Sambucus caerulea")] <- "Sambucus cerulea"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Sambucus glauca")] <- "Sambucus cerulea"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Syringa amurensis")] <- "Syringa reticulata subsp. amurensis"
+baskin_usda$Genus_species[which(baskin_egret$Genus_species == "Tsuga heterophyllad")] <- "Tsuga heterophylla"
+
+write.csv(baskin_usda,"output/baskinusdaclean.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
 
