5a39e0d686c194be2c7cb24ff289e162dd50ef24
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-02-25 15:20:24 -0800

    ids for Ken, treatments to clean in priority

--- analyses/studyDesign/decisionRules.R
+++ analyses/studyDesign/decisionRules.R
@@ -1,13 +1,6 @@
 # Started Feb 10, 2025
+# Aim: selecting some treatments/experiments to include in the model, following various decision rules
 # by Victor
-
-library(dplyr) # oops
-library(ggplot2)
-library(patchwork)
-
-# housekeeping
-rm(list=ls()) 
-options(stringsAsFactors = FALSE)
 
 if(length(grep("deirdre", getwd()) > 0)) {
   setwd("~/Documents/github/egret/analyses")
@@ -94,12 +87,16 @@ filteredd <- filteredd[!(filteredd$chemicalCor %in% chem.toremove),]
 misc.toremove <- unique(d[grepl('stress', d$other.treatment),'other.treatment'])
 filteredd <- filteredd[!(filteredd$chemicalCor %in% misc.toremove),]
 
+# For Ken: id of studies remaining at this step
+# ids <- unique(filteredd[c('datasetID', 'study', 'genusspecies')])
+# saveRDS(ids, file.path('studyDesign', 'ids_for_ken', 'ids_after_step3.rds'))
+
 # Fourth veto: no info on germination temperature
 filteredd <- filteredd[!is.na(filteredd$germTempGen),]
 
 ids <- unique(filteredd[c('datasetID', 'study', 'genusspecies')])
 ids <- ids[!(ids$datasetID == 'forbes09' & ids$study == 'exp20'),]
-ids <- ids[!(ids$datasetID == 'kolodziejek19' & ids$study == 'exp0'),] # I don't understand photoperiodCor in this study, nor cleanPhotoperiod.R, so I don't want to make any mistake
+ids <- ids[!(ids$datasetID == 'kolodziejek19' & ids$study == 'exp0'),] # I don't understand photoperiodCor in this study, nor cleanPhotoperiod.R, so I don't want to make any mistake (TO CHECK!)
 
 ids$misc.tokeep <- ids$misc.check <- NA
 ids$scarif.tokeep <- ids$scarif.check <- NA
@@ -574,76 +571,78 @@ for(i in 1:nrow(ids)){
   
 }
 
-ntreats <- newd %>%
-  dplyr::filter(!is.na(germDuration) & !is.na(germTemp)) %>%
-  dplyr::group_by(datasetID, study, genusspecies) %>%
-  dplyr::reframe(nstrat = n_distinct(dormancyTemp, dormancyDuration),
-                 ngerm = n_distinct(germTempGen, germDuration)) %>% 
-  as.data.frame()
-newids <- unique(ntreats[c('datasetID', 'study', 'genusspecies')])
+# ntreats <- newd %>%
+#   dplyr::filter(!is.na(germDuration) & !is.na(germTemp)) %>%
+#   dplyr::group_by(datasetID, study, genusspecies) %>%
+#   dplyr::reframe(nstrat = n_distinct(dormancyTemp, dormancyDuration),
+#                  ngerm = n_distinct(germTempGen, germDuration)) %>% 
+#   as.data.frame()
+# newids <- unique(ntreats[c('datasetID', 'study', 'genusspecies')])
+# 
+# newids_chill <- unique(ntreats[ntreats$nstrat > ntreats$ngerm, c('datasetID', 'study', 'genusspecies')])
+# newids_forc <- unique(ntreats[ntreats$ngerm >= ntreats$nstrat, c('datasetID', 'study', 'genusspecies')])
 
-newids_chill <- unique(ntreats[ntreats$nstrat > ntreats$ngerm, c('datasetID', 'study', 'genusspecies')])
-newids_forc <- unique(ntreats[ntreats$ngerm >= ntreats$nstrat, c('datasetID', 'study', 'genusspecies')])
+# pdf(file=paste0("figures/studyDesign/chillhours.pdf"), height = 15, width = 18)
+# par(mfrow = c(11,13), mar=c(1.4,0,0,0)+0.7, mgp=c(0,0.5,0))
+# for(i in 1:nrow(newids_chill)){
+# 
+#   di <- newd[paste0(newd$datasetID,newd$study,newd$genusspecies) == 
+#                paste0(newids_chill[i,c('datasetID', 'study', 'genusspecies')], collapse = ''),
+#              c('dormancyDuration', 'dormancyTemp', 
+#                'germTempGen', 'germDuration', 'germPhotoperiod',
+#                'responseValue')]
+#   
+#   di$chillhours <- as.numeric(di$dormancyDuration) * 24 * as.numeric(as.numeric(di$dormancyTemp) < 10 & as.numeric(di$dormancyTemp) > -20)
+#   di$forc <- as.numeric(di$germDuration) * as.numeric(di$germTempGen)
+#   
+#   plot.new()
+#   limits <- c(min(di$chillhours, na.rm = T), max(di$chillhours, na.rm = T))
+#   plot.window(xlim = limits, ylim = c(0,100))
+#   grid()
+#   
+#   points(di$responseValue ~ di$chillhours, pch = 19, col = '#498ba7', cex = 0.5)
+#   # for(f in unique(paste0(di$germTempGen, di$germDuration))){
+#   #   subi <- di[paste0(di$germTempGen, di$germDuration) == f, ]
+#   #   lines(subi$responseValue ~ subi$chillhours, col = '#498ba7')
+#   # }
+#   
+#   title(ylab = "Germ. perc.", cex.lab = 0.5)
+#   title(xlab = "Chill hours", cex.lab = 0.5, line = -0.1)
+#   title(paste("ID", i, paste(newids_chill[i,c('datasetID', 'study', 'genusspecies')], collapse = '|')), adj=0, cex.main = 0.5)
+#   
+# }
+# dev.off()
+# 
+# pdf(file=paste0("figures/studyDesign/forcingtime.pdf"), height = 15, width = 18)
+# par(mfrow = c(11,13), mar=c(1.4,0,0,0)+0.7, mgp=c(0,0.5,0))
+# for(i in 1:nrow(newids_forc)){
+# 
+#   di <- newd[paste0(newd$datasetID,newd$study,newd$genusspecies) == 
+#                paste0(newids_forc[i,c('datasetID', 'study', 'genusspecies')], collapse = ''),
+#              c('dormancyDuration', 'dormancyTemp', 
+#                'germTempGen', 'germDuration', 'germPhotoperiod',
+#                'responseValue')]
+#   
+#   di$chillhours <- as.numeric(di$dormancyDuration) * 24 * as.numeric(as.numeric(di$dormancyTemp) < 10 & as.numeric(di$dormancyTemp) > -20)
+#   di$forc <- as.numeric(di$germTempGen)
+#   di$t <- as.numeric(di$germDuration)
+#   
+#   plot.new()
+#   limits <- c(min(di$t, na.rm = T), max(di$t, na.rm = T))
+#   plot.window(xlim = limits, ylim = c(0,100))
+#   grid()
+#   
+#   points(di$responseValue ~ di$t, pch = 19, col = '#498ba7', cex = 0.5)
+#   # for(f in unique(paste0(di$germTempGen, di$germDuration))){
+#   #   subi <- di[paste0(di$germTempGen, di$germDuration) == f, ]
+#   #   lines(subi$responseValue ~ subi$chillhours, col = '#498ba7')
+#   # }
+#   
+#   title(ylab = "Germ. perc.", cex.lab = 0.5)
+#   title(xlab = "Time", cex.lab = 0.5, line = -0.1)
+#   title(paste("ID", i, paste(newids_forc[i,c('datasetID', 'study', 'genusspecies')], collapse = '|')), adj=0, cex.main = 0.5)
+#   
+# }
+# dev.off()
 
-pdf(file=paste0("figures/studyDesign/chillhours.pdf"), height = 15, width = 18)
-par(mfrow = c(11,13), mar=c(1.4,0,0,0)+0.7, mgp=c(0,0.5,0))
-for(i in 1:nrow(newids_chill)){
-  print(i)
-  di <- newd[paste0(newd$datasetID,newd$study,newd$genusspecies) == 
-               paste0(newids_chill[i,c('datasetID', 'study', 'genusspecies')], collapse = ''),
-             c('dormancyDuration', 'dormancyTemp', 
-               'germTempGen', 'germDuration', 'germPhotoperiod',
-               'responseValue')]
-  
-  di$chillhours <- as.numeric(di$dormancyDuration) * 24 * as.numeric(as.numeric(di$dormancyTemp) < 10 & as.numeric(di$dormancyTemp) > -20)
-  di$forc <- as.numeric(di$germDuration) * as.numeric(di$germTempGen)
-  
-  plot.new()
-  limits <- c(min(di$chillhours, na.rm = T), max(di$chillhours, na.rm = T))
-  plot.window(xlim = limits, ylim = c(0,100))
-  grid()
-  
-  points(di$responseValue ~ di$chillhours, pch = 19, col = '#498ba7', cex = 0.5)
-  # for(f in unique(paste0(di$germTempGen, di$germDuration))){
-  #   subi <- di[paste0(di$germTempGen, di$germDuration) == f, ]
-  #   lines(subi$responseValue ~ subi$chillhours, col = '#498ba7')
-  # }
-  
-  title(ylab = "Germ. perc.", cex.lab = 0.5)
-  title(xlab = "Chill hours", cex.lab = 0.5, line = -0.1)
-  title(paste("ID", i, paste(newids_chill[i,c('datasetID', 'study', 'genusspecies')], collapse = '|')), adj=0, cex.main = 0.5)
-  
-}
-dev.off()
-
-pdf(file=paste0("figures/studyDesign/forcingtime.pdf"), height = 15, width = 18)
-par(mfrow = c(11,13), mar=c(1.4,0,0,0)+0.7, mgp=c(0,0.5,0))
-for(i in 1:nrow(newids_forc)){
-  print(i)
-  di <- newd[paste0(newd$datasetID,newd$study,newd$genusspecies) == 
-               paste0(newids_forc[i,c('datasetID', 'study', 'genusspecies')], collapse = ''),
-             c('dormancyDuration', 'dormancyTemp', 
-               'germTempGen', 'germDuration', 'germPhotoperiod',
-               'responseValue')]
-  
-  di$chillhours <- as.numeric(di$dormancyDuration) * 24 * as.numeric(as.numeric(di$dormancyTemp) < 10 & as.numeric(di$dormancyTemp) > -20)
-  di$forc <- as.numeric(di$germTempGen)
-  di$t <- as.numeric(di$germDuration)
-  
-  plot.new()
-  limits <- c(min(di$t, na.rm = T), max(di$t, na.rm = T))
-  plot.window(xlim = limits, ylim = c(0,100))
-  grid()
-  
-  points(di$responseValue ~ di$t, pch = 19, col = '#498ba7', cex = 0.5)
-  # for(f in unique(paste0(di$germTempGen, di$germDuration))){
-  #   subi <- di[paste0(di$germTempGen, di$germDuration) == f, ]
-  #   lines(subi$responseValue ~ subi$chillhours, col = '#498ba7')
-  # }
-  
-  title(ylab = "Germ. perc.", cex.lab = 0.5)
-  title(xlab = "Time", cex.lab = 0.5, line = -0.1)
-  title(paste("ID", i, paste(newids_forc[i,c('datasetID', 'study', 'genusspecies')], collapse = '|')), adj=0, cex.main = 0.5)
-  
-}
-dev.off()
+rm(list=ls()[which(!(ls() %in% c('d', 'newd')))]) 
