2b573c59d96f8ba893b68d5f765419c7bc6c5fb1
wangxm-forest <wangxiaomao0209@gmail.com>
2025-04-05 15:22:10 -0700

    Updated rescraped dataset

--- analyses/cleaning/source/cleanMisc.R
+++ analyses/cleaning/source/cleanMisc.R
@@ -105,6 +105,12 @@ d$study[which(d$datasetID == "li11" & d$study == "exp 1 ")] <- "exp 1"
 # rescraped jusung16, Table 1
 d <- d[-which(d$datasetID == "jusung16" & d$entered.by == "SC" & d$figure == "Table 1"),]  
 
+# rescraped tang10b
+d <- d[-which(d$datasetID == "tang10b" & d$entered.by == "JN"),]
+
+# rescraped bungard97 Fig1B
+d <- d[-which(d$datasetID == "bungard97" & d$entered.by == "BW" & d$figure == "Figure 1b"),]
+
 if(FALSE){
 ##	
 ## From clean_other.R, Original file called coordinate_cleaning_JS.R
