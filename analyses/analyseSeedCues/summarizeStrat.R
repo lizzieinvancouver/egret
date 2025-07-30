82eeba45eed038424a85748d0383a29806856546
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-07-30 11:55:42 -0700

    correcting quite a big typo + adding zhou03 particular case

--- analyses/analyseSeedCues/summarizeStrat.R
+++ analyses/analyseSeedCues/summarizeStrat.R
@@ -1,6 +1,16 @@
 
 # Summarizing stratification treatments in a more handy format
 
+# modifying chilling in tang21
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "1344 and 1344", 'chillDuration'] <- '56 then 56'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "1344 and 1344", 'chillTemp'] <- '4 then 22'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "672 and 672", 'chillDuration'] <- '28 then 28 then 28 then 28'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "672 and 672", 'chillTemp'] <- '4 then 22 then 4 then 22'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "336 and 336", 'chillDuration'] <- '14 then 14 then 14 then 14 then 14 then 14 then 14 then 14'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "336 and 336", 'chillTemp'] <- '4 then 22 then 4 then 22 then 4 then 22 then 4 then 22'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "168 and 168", 'chillDuration'] <- '7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7 then 7'
+d[d$datasetID %in% 'zhou03' & d$study %in% 'exp1' & d$chillTempCycle %in% "168 and 168", 'chillTemp'] <- '4 then 22 then 4 then 22 then 4 then 22 then 4 then 22 then 4 then 22 then 4 then 22 then 4 then 22 then 4 then 22'
+
 # detailed info
 cold_range <- c(-10, 10)
 warm_range <- c(10, 40) # exclude 10
@@ -108,7 +118,7 @@ for(i in 1:nrow(d)){
   # lookup into chilling conditions
   chillTemp_i <- unlist(stringr::str_split(d$chillTemp[i], " then "))
   chillDur_i <- unlist(stringr::str_split(d$chillDuration[i], " then "))
-  for(n in 1:length(storTemp_i)){
+  for(n in 1:length(chillTemp_i)){
     len <- length(stratSequence_i)
     if(!is.na(as.numeric(chillTemp_i[n])) & !is.na(as.numeric(chillDur_i[n]))){                                                                                                  
       if(as.numeric(chillTemp_i[n]) > warm_range[1] & as.numeric(chillTemp_i[n]) <= warm_range[2]){
@@ -198,36 +208,36 @@ d$warmStratTemp <- as.numeric(sapply(1:nrow(d), function(i){
 }))
 
 # If you want to plot 
-strat_summary <- data.frame(table(unique(d[,c('datasetID', 'stratSequence_condensed')])$stratSequence_condensed))
-names(strat_summary) <- c('sequence', 'count')
-strat_summary_plotdf <- data.frame()
-for(i in 1:nrow(strat_summary)){
-  seq <- unlist(stringr::str_split(strat_summary$sequence[i], ' then '))
-  seqplot <- rep(NA, 3)
-  if(length(seq) == 1){
-    seqplot[3] <- seq[1]
-  }else if(length(seq) == 2){
-    seqplot[2:3] <- seq[1:2]
-  }else{
-    seqplot <- seq
-  }
-  names(seqplot) <- paste0('step', 1:3)
-  strat_summary_plotdf <- rbind(
-    strat_summary_plotdf,
-    data.frame(count = strat_summary$count[i], t(seqplot))
-  )
-}
-strat_summary_plotdf <- strat_summary_plotdf[order(strat_summary_plotdf$count),]
-strat_summary_plotdf$y <- 1:nrow(strat_summary_plotdf)
-strat_summary_plot <- ggplot(data = strat_summary_plotdf) +
-  geom_point(aes(y = y, x = 1, color = step1), size = 5) +
-  geom_point(aes(y = y, x = 1.5, color = step2), size = 5) +
-  geom_point(aes(y = y, x = 2, color = step3), size = 5) +
-  geom_text(aes(y = y, x = 0.3, label = paste0('n=',count))) +
-  scale_color_manual(breaks = c('undefined', 'cold', 'warm'), values = c('grey80', '#178a94', '#ee8080'), na.value = NA) +
-  theme_void() +
-  theme(legend.position = 'bottom',
-        legend.title = element_blank(),
-        plot.margin = margin(5,5,5,10)) +
-  guides(color = guide_legend(override.aes = list(size = 2))) +
-  coord_cartesian(xlim = c(0,2))
+# strat_summary <- data.frame(table(unique(d[,c('datasetID', 'stratSequence_condensed')])$stratSequence_condensed))
+# names(strat_summary) <- c('sequence', 'count')
+# strat_summary_plotdf <- data.frame()
+# for(i in 1:nrow(strat_summary)){
+#   seq <- unlist(stringr::str_split(strat_summary$sequence[i], ' then '))
+#   seqplot <- rep(NA, 3)
+#   if(length(seq) == 1){
+#     seqplot[3] <- seq[1]
+#   }else if(length(seq) == 2){
+#     seqplot[2:3] <- seq[1:2]
+#   }else{
+#     seqplot <- seq
+#   }
+#   names(seqplot) <- paste0('step', 1:3)
+#   strat_summary_plotdf <- rbind(
+#     strat_summary_plotdf,
+#     data.frame(count = strat_summary$count[i], t(seqplot))
+#   )
+# }
+# strat_summary_plotdf <- strat_summary_plotdf[order(strat_summary_plotdf$count),]
+# strat_summary_plotdf$y <- 1:nrow(strat_summary_plotdf)
+# strat_summary_plot <- ggplot(data = strat_summary_plotdf) +
+#   geom_point(aes(y = y, x = 1, color = step1), size = 5) +
+#   geom_point(aes(y = y, x = 1.5, color = step2), size = 5) +
+#   geom_point(aes(y = y, x = 2, color = step3), size = 5) +
+#   geom_text(aes(y = y, x = 0.3, label = paste0('n=',count))) +
+#   scale_color_manual(breaks = c('undefined', 'cold', 'warm'), values = c('grey80', '#178a94', '#ee8080'), na.value = NA) +
+#   theme_void() +
+#   theme(legend.position = 'bottom',
+#         legend.title = element_blank(),
+#         plot.margin = margin(5,5,5,10)) +
+#   guides(color = guide_legend(override.aes = list(size = 2))) +
+#   coord_cartesian(xlim = c(0,2))
